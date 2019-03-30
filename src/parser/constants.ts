8afc86b985c578115e386354242c4afaead4b9ee
Aamir Maniar <aamironline@gmail.com>
2019-03-30 19:32:25 +0530

    Reorganize constants!

--- src/parser/constants.ts
+++ src/parser/constants.ts
@@ -1,14 +1,20 @@
 
-// Constants
+// CharCodes
 export const ZERO = 48
 export const NINE = 57
 export const SPACE = 32
-export const TRUE = "T"
-export const FALSE = "F"
+
+// Other
 export const KEY = "key"
 export const VAL = "val"
 export const UNSURE = "unsure"
 export const EMPTY = "empty"
-export const STRING_ENCLOSER = '"'
 
+// Keywords and Language
+export const TRUE = "T"
+export const FALSE = "F"
+export const NULL = "N"
+export const DATASEP = "---"
+
+export const STRING_ENCLOSER = '"'
 export const SEPARATORS = [',', ':', '{', '}', '[', ']']
