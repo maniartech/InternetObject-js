1769e8b5023b23da5484959a18c5023f99d13f64
Aamir Maniar <aamironline@gmail.com>
2019-04-20 12:28:38 +0530

    DataParser correctly identifies scalar values

--- src/parser/data-parser.ts
+++ src/parser/data-parser.ts
@@ -1,5 +1,7 @@
 import { isDataType, isArray, isParserTree, isKeyVal, isToken } from '../utils/is'
 import { ASTParserTree } from '.'
+import { print } from '../utils';
+
 
 /**
  * Parses the ASTParserTree into the data that makes sense.
@@ -7,36 +9,42 @@ import { ASTParserTree } from '.'
 export default class DataParser {
   public static parse(dataTree:ASTParserTree) {
 
-    if (dataTree.values.length === 0) {
-      return
-    } else if (dataTree.values.length === 1) {
+    if (dataTree.type === "scalar") {
       const val = dataTree.values[0]
       if (isToken(val)) {
         return val.value
       }
+      if (val === null) {
+        return
+      }
     }
 
-    return generateObject(dataTree, {})
+    const container = dataTree.type === "object" ? {} : []
+    if (dataTree.values.length === 0) {
+      return container
+    }
+
+    return _parse(dataTree, container)
   }
 }
 
-const generateObject = (root:ASTParserTree, container:any) => {
+const _parse = (root:ASTParserTree, container:any) => {
   for (let index=0; index < root.values.length; index += 1) {
     let value = root.values[index]
 
     if (isParserTree(value)) {
       // Object
       if(value.type === 'object') {
-        container[index] = generateObject(value, {})
+        container[index] = _parse(value, {})
       }
       // Array
       else if(value.type === 'array') {
-        container[index] = generateObject(value, [])
+        container[index] = _parse(value, [])
       }
     }
     else if(isKeyVal(value)) {
       if (isParserTree(value.value)) {
-        container[value.key] = generateObject(value.value,
+        container[value.key] = _parse(value.value,
           value.value.type === "object" ? {} : [])
       }
       else if (isKeyVal(value.value)) {
