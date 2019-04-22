feca65d0b3ccdb01b61f1f9ee651eed2c62a56ab
Aamir Maniar <aamironline@gmail.com>
2019-04-22 11:29:23 +0530

    Improve ErrorCodes

--- src/errors/io-error-codes.ts
+++ src/errors/io-error-codes.ts
@@ -1,14 +1,30 @@
-enum IObjectErrorCodes {
+enum ErrorCodes {
 
-  invalidType = "invalid-type",
-  tokenNotReady = "token-not-ready",
-  invalidValue = "invalid-value",
-  invalidChoice = "invalid-choice",
+  // General
+  invalidType       = "invalid-type",
+  tokenNotReady     = "token-not-ready",
+  invalidValue      = "invalid-value",
+
+  // -- string, number, date, datetime, time
+  valueNotInChoice  = "value-not-in-choice",
+
+  // -- string, array
+  invalidMinLength  = "invalid-min-length",
+  invalidMaxLength  = "invalid-max-length",
 
   // Number
-  notANumber      = "not-a-number",
-  invalidMinValue = "invalid-min-value",
-  invalidMaxValue = "invalid-max-value",
+  notANumber        = "not-a-number",
+  invalidMinValue   = "invalid-min-value",
+  invalidMaxValue   = "invalid-max-value",
+
+  // String
+  notAString        = "not-a-string",
+
+
+  // Object
+
+
+  // Array
 }
 
-export default IObjectErrorCodes
+export default ErrorCodes
