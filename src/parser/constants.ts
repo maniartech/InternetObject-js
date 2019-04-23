ae9310f91d686e138e695fc4eb7de749686b9f47
Aamir Maniar <aamironline@gmail.com>
2019-04-23 09:53:28 +0530

    Update constants

--- src/parser/constants.ts
+++ src/parser/constants.ts
@@ -9,6 +9,8 @@ export const KEY = "key"
 export const VAL = "val"
 export const UNSURE = "unsure"
 export const EMPTY = "empty"
+export const HYPHEN = "-"
+export const NEW_LINE = '\n'
 
 // Keywords and Language
 export const TRUE = "T"
@@ -16,5 +18,19 @@ export const FALSE = "F"
 export const NULL = "N"
 export const DATASEP = "---"
 
+// Separators
+export const TILDE = "~"
+export const COMMA = ","
+export const COLON = ":"
+export const CURLY_OPEN = "{"
+export const CURLY_CLOSED = "}"
+export const SQUARE_OPEN = "["
+export const SQUARE_CLOSED = "]"
+
 export const STRING_ENCLOSER = '"'
-export const SEPARATORS = [',', ':', '{', '}', '[', ']']
+export const SEPARATORS = [
+                COMMA, COLON,
+                CURLY_OPEN, CURLY_CLOSED,
+                SQUARE_OPEN, SQUARE_CLOSED,
+                TILDE
+              ]
