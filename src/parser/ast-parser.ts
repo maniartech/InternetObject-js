76a7b63bee472dd72d219d2dddb002a51246bcad
Aamir Maniar <aamironline@gmail.com>
2019-04-25 22:37:13 +0530

    ASPParser considers collections & headers (WIP)

--- src/parser/ast-parser.ts
+++ src/parser/ast-parser.ts
@@ -1,12 +1,11 @@
-import { Token } from './token'
-import { isDataType, isArray, isParserTree, isKeyVal } from '../utils/is'
-import { ASTParserTree, ParserTreeValue } from '.'
-// import SchemaParser from './bak-schema-compiler'
-import DataParser from './data-parser'
-import { print } from '../utils/index'
-import InternetObjectSchema from '../schema'
-import Tokenizer from './tokenizer'
 import InternetObjectError from '../errors/io-error';
+import { print } from '../utils/index';
+import { isKeyVal, isSchemaDef } from '../utils/is';
+import { ASTParserTree, ParserTreeValue } from './';
+import { Token } from './token';
+import Tokenizer from './tokenizer';
+import { COMMA, CURLY_CLOSED, SQUARE_CLOSED, TILDE } from './constants';
+import ErrorCodes from '../errors/io-error-codes';
 
 const NOT_STARTED = 'not-started'
 const STARTED = 'started'
@@ -16,15 +15,17 @@ const ERROR = 'error'
 /** @internal */
 export default class ASTParser {
   private _tokenizer: Tokenizer
-  private _schemaOnly:boolean
-  private _text:string
+  private _schemaOnly: boolean
+  private _text: string
   public stack: ASTParserTree[] = []
   private lastToken: Token | null = null
+  private _isCollection = false
+  private _isHeaderOpen = true
 
   public status = NOT_STARTED
   public tree: any = {}
 
-  constructor(text: string, schemaOnly:boolean = false) {
+  constructor(text: string, schemaOnly: boolean = false) {
     this._text = text
     this._tokenizer = new Tokenizer(text)
     this._schemaOnly = schemaOnly
@@ -43,7 +44,7 @@ export default class ASTParser {
     if (this.status !== FINISHED) {
       throw new Error('parsing-not-finished')
     }
-    return this.tree.schema
+    return this.tree.header
   }
 
   public get data() {
@@ -53,40 +54,25 @@ export default class ASTParser {
     return this.tree.data
   }
 
-  public toObject = () => {
-    if (this.stack.length === 0) return null
-    const data = this.tree.data
-    if (data) {
-      return DataParser.parse(data)
-    }
-    return null
-  }
-
-  public toSchema = () => {
-    if (this.stack.length === 0) return null
-    const schema = (this.tree.header || {}).schema
-      ? this.tree.header.schema
-      : this.tree.data || null
-    // console.log("...", schema)
-
-    return null
-  }
-
+  // Process the tokens and build ast.
   private _process = (token: Token, isFinalToken: boolean = false) => {
     let stack = this.stack
-    let obj = this._getObject()
 
     this.status = STARTED
 
+    // Push new object
     if (token.value === '{') {
-      // this.stack.push("{")
-      this._push()
-      // return
-    } else if (token.value === '[') {
-      // this.stack.push("[")
-      this._push('array')
-      // return
-    } else if (token.value === ':') {
+      this._push("object")
+    }
+
+    // Push new array
+    else if (token.value === '[') {
+      this._push("array")
+    }
+
+    // Key Value found
+    else if (token.value === ':') {
+      const obj = this._getObject()
       let lastVal: any = obj.values[obj.values.length - 1]
 
       if (lastVal && ['string', 'number', 'boolean'].indexOf(typeof lastVal.value) > -1) {
@@ -100,27 +86,55 @@ export default class ASTParser {
         // Throw Invalid key error!
       }
     }
-    // Empty token
-    else if (token.value === ',') {
+
+    // New Token
+    else if (token.value === COMMA) {
+      // Empty Token Found
       if (this.lastToken !== null && this.lastToken.value === ',') {
         this._addValue(null)
       }
-    } else if (token.value === '}' || token.value === ']') {
-      this._pop(token.value === '}' ? 'object' : 'array', token)
+    }
+
+    // Close object and close array
+    else if (token.value === CURLY_CLOSED || token.value === SQUARE_CLOSED) {
+      this._pop(token.value === CURLY_CLOSED ? 'object' : 'array', token)
+    }
+
+    else if (token.value === TILDE) {
+      // The ~ sign can occur in any section (header/data) first.
+      // If it is a new tree, or header/schema is already been established,
+      // set the collection mode to true.
+      if (this.stack.length === 0) {
+        this._isCollection = true
+      }
+      // When the TILDE is encountered second time check collection mode is on,
+      // if not, throw an error
+      else if (!this._isCollection && !this.tree.data) {
+        // TODO: Throw and error here
+        console.log(this.tree)
+        console.log(this.stack)
+        throw new InternetObjectError(ErrorCodes.invalidCollection, null, token)
+      }
+      else {
+        this._processObject()
+      }
+
     }
 
     // Ready for schema generation
     else if (token.type === 'datasep') {
-      if (!this.tree.schema) {
-        // TODO: Consider the scenario when more than 1 item exists in the stack!
-        if (this.stack.length > 1) {
-          console.warn("Invalid Stack")
-        }
-        this.tree.schema = { ...this.stack[0] }
-        this.stack.length = 0
+      // Header is closed when, datasep is found. After the header
+      // is closed, throw an error when another datasep is sent!
+      if (!this._isHeaderOpen) {
+        throw new InternetObjectError(ErrorCodes.multipleHeaders,"Multiple headers are not allowed.", token)
       }
+      this._processObject()
+      this._isHeaderOpen = false
+      this._isCollection = false
+      this.stack.length = 0
+      // this._addToHeader()
+
     }
-    // else if(token.type !== "sep") {
     else {
       this._addValue(token)
     }
@@ -128,8 +142,68 @@ export default class ASTParser {
     this.lastToken = token
   }
 
+  private _processObject = () => {
+    const isCollection = this._isCollection
+    const isHeaderOpen = this._isHeaderOpen
+    const data = this.stack[0]
+
+    // Nothing to process
+    if (this.stack.length === 0) {
+      // TODO: Handle this case
+    }
+
+    // When there are open objects and arrays, stack will contain
+    // more than one items.
+    if (this.stack.length > 1) {
+      // TODO: Throw a proper error here
+      throw new Error("open-bracket")
+    }
+
+    // Process header section
+    if (isHeaderOpen) {
+      if (isCollection) {
+        if (!this.tree.header) {
+          this.tree.header = [data]
+        }
+        else {
+          this.tree.header.push(data)
+        }
+      }
+      else {
+        this.tree.header = data
+      }
+    }
+    else {
+      // Process data section
+      if (isCollection) {
+        if (!this.tree.data) {
+          this.tree.data = [data]
+        }
+        else {
+          this.tree.data.push(data)
+        }
+      }
+      else {
+        this.tree.data = data
+      }
+    }
+
+    // Reset stack
+    this.stack.length = 0
+  }
+
   private _finalize = () => {
     this.status = FINISHED
+
+    this._processObject()
+
+    if (this._isHeaderOpen) {
+      this.tree.data = this.tree.header
+      delete this.tree.header
+    }
+
+
+    return
 
     // If no data found just return
     if (this.stack.length === 0) return
@@ -146,7 +220,7 @@ export default class ASTParser {
 
     if (this.tree.data === undefined) {
       if (this._schemaOnly) {
-        this.tree.schema = data
+        this.tree.header = data
         // TODO: Handle the case when compiling for schema
         // and token count is 1
         console.warn("Compiling for schema while token count is 1")
@@ -158,6 +232,35 @@ export default class ASTParser {
         this.tree.data = data
       }
     }
+  }
+
+  private _addToHeader = () => {
+    if (!this.tree.header) {
+      // When more than 1 item exists in the stack, it means
+      // one or more bracket is still open.
+      if (this.stack.length > 1) {
+        // TODO: Throw a proper error
+        console.warn("Invalid Stack")
+      }
+      this.tree.header = this.stack[0]
+      this.stack.length = 0
+    }
+    else {
+      // TODO: Handle this case!
+      console.warn("multiple-headers")
+    }
+  }
+
+  private _getObject = () => {
+    if (this.stack.length === 0) {
+      this.stack.push({
+        type: 'object',
+        values: []
+      })
+    }
+
+    let obj = this.stack[this.stack.length - 1]
+    return obj
   }
 
   private _addValue = (value: any) => {
@@ -179,18 +282,6 @@ export default class ASTParser {
     }
   }
 
-  private _getObject = () => {
-    if (this.stack.length === 0) {
-      this.stack.push({
-        type: 'object',
-        values: []
-      })
-    }
-
-    let obj = this.stack[this.stack.length - 1]
-    return obj
-  }
-
   private _push = (type = 'object', values = []) => {
     let object = this._getObject()
     let newObject = { type, values }
@@ -204,7 +295,7 @@ export default class ASTParser {
     if (obj.type !== type) {
       const message = `Expecting "${obj.type === 'object' ? '}' : ']'}" but found "${
         obj.type === 'object' ? ']' : '}'
-      }"`
+        }"`
       this.status = ERROR
       // throwError(token, message)
       throw new InternetObjectError("parser-error", message, token)
