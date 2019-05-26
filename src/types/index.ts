0f98bed6b92c533d5f9b098738a779faab64f42e
Aamir Maniar <aamironline@gmail.com>
2019-05-26 10:32:52 +0530

    #12 added int, byte, int16 & int32 subtypes

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,7 +10,13 @@ import TypedefRegistry from './typedef-registry';
 
 TypedefRegistry.register("any", new AnyDef())
 TypedefRegistry.register("string", new StringDef())
+
 TypedefRegistry.register("number", new NumberDef())
+TypedefRegistry.register("byte", new NumberDef("byte"))
+TypedefRegistry.register("int16", new NumberDef("int16"))
+TypedefRegistry.register("int32", new NumberDef("int32"))
+TypedefRegistry.register("int", new NumberDef("int"))
+
 
 TypedefRegistry.register("object", new ObjectDef())
 TypedefRegistry.register("array", new ArrayDef())
