d67343a617243d1363b248932aa5da0c1bc5cc73
Aamir Maniar <aamironline@gmail.com>
2019-07-14 16:24:25 +0530

    Error class improved

--- src/errors/io-error.ts
+++ src/errors/io-error.ts
@@ -1,6 +1,15 @@
-import { Token } from "../parser";
+import { Node } from "../parser";
+import InternetObject from "../index";
 
+// Error Examples
+// InternetObject (SyntaxError): invalid-value at 1, 20
+// InternetObject (SyntaxError): invalid-value
+// InternetObject (ValidationError): invalid-choice - The value of 'address.city' is not correct at 1, 20.
+// It must be one of these values [Mumbai, Thane, Pune].
 
+/**
+ * Represents the base class for throwing InternetObjectError.
+ */
 export default class InternetObjectError extends Error {
 
   // Due to a bug in TypeScript specifically control the __proto__
@@ -9,30 +18,79 @@ export default class InternetObjectError extends Error {
   // tslint:disable-next-line:variable-name
   public __proto__: Error;
 
+  /**
+   * Error code of internet object error.
+   */
   public errorCode?:string
 
-  constructor(...args:any[]) {
+  /**
+   * Line number of internet object error
+   */
+  public lineNumber?:number
+
+  /**
+   * Column number of internet object error
+   */
+  public columnNumber?:number
+
+  /**
+   * Index  of internet object error
+   */
+  public index?:number
+
+
+  /**
+   *
+   * @param errorCode {string} The errorCode
+   * @param message {string} The error message
+   * @param node {Node} Optional
+   * @param ssf
+   */
+  constructor(errorCode:string, message:string="", node?:Node, ssf?:any) {
     super()
 
-    const errorCode:string = args[0] || ""
-    const message:string = args[1] || ""
-    const token:Token|null = args[2] || null
     let errorMsg:string = errorCode
 
     this.errorCode = errorCode
 
-    if (token) {
-      errorMsg = `${errorCode} at (${token.row}, ${token.col}): ${message}`
+    if (node) {
+      this.lineNumber = node.row
+      this.columnNumber = node.col
+      this.index = node.index
+      errorMsg = `${errorCode} at (${node.row}, ${node.col}): ${message}`
     }
     else {
       errorMsg = message ? `${errorCode}: ${message}` : errorCode
     }
     this.message = errorMsg
 
-    Error.captureStackTrace(this, InternetObjectError)
+    Error.captureStackTrace(this, ssf || InternetObject)
     this.__proto__ = new.target.prototype
+  }
+}
+
+/**
+ * Represents the syntax error in InternetObject. This error is thrown and a systax error
+ * is occured while parting the InternetObject document or schema.
+ */
+export class InternetObjectSyntaxError extends InternetObjectError {
+
+  constructor (errorCode:string, message:string="", node?:Node, ssf?:any) {
+    super(errorCode, message, node, ssf)
+    this.name = "InternetObjectSyntaxError"
+  }
+}
+
+/**
+ * Represents the validation error in InternetObject. This error is thrown when a validation
+ * issue is found while validating the internet-object data against the associated schema.
+ */
+export class InternetObjectValidationError extends InternetObjectError {
+
+  constructor (errorCode:string, message:string="", node?:Node, ssf?:any) {
+    super(errorCode, message, node, ssf)
+    this.name = "InternetObjectValidationError"
   }
 
 }
-
 
