98a5e209977c6b1223e23ed7540283d99f2b535c
Aamir Maniar <aamironline@gmail.com>
2019-08-17 12:39:12 +0530

    Added support for loading of POJO objects

--- src/types/typedef.ts
+++ src/types/typedef.ts
@@ -14,5 +14,9 @@ export default interface TypeDef {
    */
   parse: (value:ParserTreeValue, memberDef: MemberDef) => any
 
-
+  /**
+   * Validates and loads the value as per the memberDef and returns the results
+   * specifying whether the value adhers to the schema or not!
+   */
+   load: (value:any, memberDef: MemberDef) => any
 }
