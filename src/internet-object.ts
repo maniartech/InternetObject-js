5d8cbe07914ac072cf1a0466f1b087c72c5bb466
Aamir Maniar <aamironline@gmail.com>
2019-09-18 17:48:48 +0530

    Restructured the class

--- src/internet-object.ts
+++ src/internet-object.ts
@@ -1,26 +1,32 @@
+import Header from './header'
+import DataParser from './data'
+import Schema from './header/schema'
+import ASTParser from './parser/ast-parser'
+import ErrorCodes from './errors/io-error-codes'
 
-import DataParser from './data';
-import Header from './header';
-import Schema from './header/schema';
-// import './constants'
-import ASTParser from './parser/ast-parser';
-import { print } from './utils';
-import { isString, isArray } from './utils/is';
-import { InternetObjectError } from './errors/io-error';
-import ErrorCodes from './errors/io-error-codes';
-import { SCHEMA } from './parser/constants';
+import { isString } from './utils/is'
+import { SCHEMA } from './parser/constants'
+import { InternetObjectError } from './errors/io-error'
 
 /**
- * Represents the InternetObject. A class that is
+ * Represents the InternetObject. A main facade class that
+ * takes care of parsing, schema processing and other
+ * core responsibilities.
+ *
+ * @public
  */
 export class InternetObject<T = any> {
-
-  constructor(o:any, schema?:string|Schema) {
+  /**
+   * Initializes the new instance of the `InternetObject`
+   * @param o {string, object} An internet object text or POJO which needs to be parsed or loaded
+   * @param schema {string, Schema} An optional `Schema` object or its `string` representation
+   */
+  constructor(o: any, schema?: string | Schema) {
     let header: Header
     let data: any
 
     if (isString(o)) {
-      const parsed =_parse(o, schema)
+      const parsed = _parse(o, schema)
       header = parsed.header
       data = parsed.data
     } else {
@@ -32,22 +38,31 @@ export class InternetObject<T = any> {
     this._header = header
   }
 
-  private _header:Header
-  public get header() : Header {
+  private _header: Header
+  /**
+   * Gets the header section of the object.
+   */
+  public get header(): Header {
     return this._header
   }
 
+  /**
+   * Gets the schema associated with the object.
+   */
   public get schema() {
     return this._header.schema
   }
 
-  private _data:T
-  public get data():T {
+  private _data: T
+  /**
+   * Gets the data section of the object
+   */
+  public get data(): T {
     return this._data
   }
 }
 
-function _getCompiledSchema(schema?: string|Schema) {
+function _getCompiledSchema(schema?: string | Schema) {
   if (!schema) return null
 
   if (schema instanceof Schema) {
@@ -57,117 +72,56 @@ function _getCompiledSchema(schema?: string|Schema) {
   return Schema.compile(schema)
 }
 
-function _parseObject(o:any, schema?:string|Schema) {
+function _parseObject(o: any, schema?: string | Schema) {
   const compiledSchema = _getCompiledSchema(schema)
 
   if (compiledSchema === null) {
     return {
       data: o,
-      header: new Header
+      header: new Header()
     }
   }
 
   return {
     data: compiledSchema.apply(o),
-    header: (new Header).set(SCHEMA, compiledSchema)
+    header: new Header().set(SCHEMA, compiledSchema)
   }
-
 }
 
-
 // Parses the Internet Object string and returns the value
-function _parse(text:string, schema?:string|Schema) {
+function _parse(text: string, schema?: string | Schema) {
   const parser = new ASTParser(text)
 
-  let compiledSchema:Schema|null = null
-  let compiledHeader:Header|null = null
+  let compiledSchema: Schema | null = null
+  let compiledHeader: Header | null = null
 
   // Parse the text
   parser.parse()
 
   if (schema) {
-    if(isString(schema)) {
+    if (isString(schema)) {
       compiledSchema = Schema.compile(schema)
-    }
-    else if (schema instanceof Schema) {
+    } else if (schema instanceof Schema) {
       compiledSchema = schema
-    }
-    else {
+    } else {
       // TODO: Throw better error
       throw new InternetObjectError(ErrorCodes.invalidSchema)
     }
   }
 
-  let header:Header
-  if(parser.tree.header) {
-    header = compiledSchema === null
-      ? Header.compile(parser.tree.header)
-      : Header.compile(parser.tree.header, compiledSchema)
+  let header: Header
+  if (parser.header) {
+    header =
+      compiledSchema === null
+        ? Header.compile(parser.header)
+        : Header.compile(parser.header, compiledSchema)
 
     compiledSchema = header.schema ? header.schema : compiledSchema
-  }
-  else {
-    header = compiledSchema === null
-      ? new Header
-      : (new Header).set(SCHEMA, compiledSchema)
+  } else {
+    header = compiledSchema === null ? new Header() : new Header().set(SCHEMA, compiledSchema)
   }
 
-  const data = compiledSchema
-          ? compiledSchema.apply(parser.tree.data)
-          : DataParser.parse(parser.tree.data)
+  const data = compiledSchema ? compiledSchema.apply(parser.data) : DataParser.parse(parser.data)
 
-  return {header, data}
-
-
-
-  // if (header) {
-
-  // }
-  // else if (schema) {
-  //   // If schema is string compile it and apply
-  //   if (isString(schema)) {
-  //     const astSchmeaParser = new ASTParser(schema, true)
-  //     astSchmeaParser.parse()
-  //     const parser.header = astSchmeaParser.header
-  //     if(parser.header) {
-  //       const header = Header.compile(parser.header)
-  //       compiledSchema = header.schema
-  //     }
-  //   }
-  //   // If schema is already compiled, prioratize it over schema passed in text
-  //   else if (schema instanceof  Schema) {
-  //     compiledSchema = schema
-  //   }
-  //   else {
-  //     // TODO: Throw better error
-  //     throw new InternetObjectError(ErrorCodes.invalidSchema)
-  //   }
-  // }
-  // // If object contains schema, parse and compile it
-  // else if (!schema && parser.header) {
-  //   const header = Header.compile(parser.header)
-  //   print("---", header.length)
-  //   compiledSchema = header.schema
-  //   // compiledSchema = Schema.compile(parser.h)
-  // }
-
-  // const iObject = new InternetObject()
-  // if (compiledSchema === null) {
-  //   iObject._data = DataParser.parse(data)
-  //   iObject._schema = null
-  //   return iObject
-  // }
-
-  // // compiledSchema.apply(data)
-  // iObject._schema = compiledSchema
-  // iObject._data = compiledSchema.apply(data)
-  // return iObject
+  return { header, data }
 }
-
-function _compileSchema(text:string): Schema {
-  const header = Header.compile(text)
-  return header.schema
-}
-
-
-
