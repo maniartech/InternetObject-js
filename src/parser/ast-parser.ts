52f73b4cdb854758a2e728bd14fb1c14b24e6b05
Aamir Maniar <aamironline@gmail.com>
2019-09-18 10:01:11 +0530

    Code cleanup

--- src/parser/ast-parser.ts
+++ src/parser/ast-parser.ts
@@ -1,35 +1,51 @@
-import { print } from '../utils/index';
-import { isKeyVal, isSchemaDef, isToken } from '../utils/is';
-import { Token, ASTParserTree, ParserTreeValue } from './';
-import Tokenizer from './tokenizer';
-import { COMMA, CURLY_CLOSED, SQUARE_CLOSED, TILDE } from './constants';
-import ErrorCodes from '../errors/io-error-codes';
-import { InternetObjectSyntaxError } from '../errors/io-error';
+import Tokenizer from './tokenizer'
+import ErrorCodes from '../errors/io-error-codes'
+
+import { InternetObjectSyntaxError } from '../errors/io-error'
+import { isKeyVal, isToken } from '../utils/is'
+import { ASTParserTree, Token } from './'
+import { COMMA, CURLY_CLOSED, SQUARE_CLOSED, TILDE } from './constants'
 
 const NOT_STARTED = 'not-started'
 const STARTED = 'started'
 const FINISHED = 'finished'
 const ERROR = 'error'
 
-/** @internal */
+/**
+ * Represents the which accepts the raw text and generates abstract syntax tree.
+ * The format of the generated AST is somewhat similar to the standard one,
+ * but not the same.
+ *
+ * @internal
+ */
 export default class ASTParser {
-  private _tokenizer: Tokenizer
-  private _schemaOnly: boolean
-  private _text: string
-  public stack: ASTParserTree[] = []
-  private lastToken: Token | null = null
   private _isCollection = false
   private _isHeaderOpen = true
+  private _lastToken?: Token = undefined
+  private _isSchema: boolean
+  private _stack: ASTParserTree[] = []
+  private _text: string
+  private _tokenizer: Tokenizer
+  private _tree: any = {}
 
+  // TODO: Don't use public fields, use btter appraoch
+  // such as readonly properties like parser.isFinished...
   public status = NOT_STARTED
-  public tree: any = {}
 
-  constructor(text: string, schemaOnly: boolean = false) {
+  /**
+   * Initializes the new instance of `ASTParser` class.
+   * @param text {string} The text which needs to be parsed
+   * @param isSchema {boolean} Whether the text is schema or not, `true` if it is schema.
+   */
+  constructor(text: string, isSchema: boolean = false) {
     this._text = text
     this._tokenizer = new Tokenizer(text)
-    this._schemaOnly = schemaOnly
+    this._isSchema = isSchema
   }
 
+  /**
+   * Parses the text.
+   */
   parse = () => {
     let token = this._tokenizer.read()
     while (token) {
@@ -40,53 +56,73 @@ export default class ASTParser {
     // print("Parser", this)
   }
 
+  /**
+   * Returns the header part. Throws an error if invoked
+   * before the parsing is finished!
+   */
   public get header() {
     if (this.status !== FINISHED) {
       throw new Error('parsing-not-finished')
     }
-    return this.tree.header
+    return this._tree.header
   }
 
+  /**
+   * Returns the data part. Throws an error if invoked
+   * before the parsing is finished!
+   */
   public get data() {
     if (this.status !== FINISHED) {
       throw new Error('parsing-not-finished')
     }
-    return this.tree.data
+    return this._tree.data
+  }
+
+  /**
+   * Returns the text this object has parsed or about
+   * to be parsed.
+   */
+  public get text() {
+    return this._text
   }
 
   // Process the tokens and build ast.
   private _process = (token: Token, isFinalToken: boolean = false) => {
-    let stack = this.stack
+    let stack = this._stack
 
     this.status = STARTED
 
     // Push new object
     if (token.value === '{') {
-      this._push("object", token.index, token.row, token.col)
+      this._push('object', token.index, token.row, token.col)
+      // console.log("#####", this._stack)
     }
 
     // Push new array
     else if (token.value === '[') {
-      this._push("array", token.index, token.row, token.col)
+      this._push('array', token.index, token.row, token.col)
     }
 
     // When a colon is found, consider previous value as key
     // and keep the value slot to null. When the next token arives, push it
     // into the value slot.
+    // TODO: Todo check key-value pair is complete.
     else if (token.value === ':') {
       const obj = this._getObject()
       // TODO: Validate length of the obj.values
-      let lastToken:any = obj.values[obj.values.length - 1]
+      let lastToken: any = obj.values[obj.values.length - 1]
 
-      if (isToken(lastToken) && ['string', 'number', 'boolean'].indexOf(typeof lastToken.value) > -1) {
+      if (
+        isToken(lastToken) &&
+        ['string', 'number', 'boolean'].indexOf(typeof lastToken.value) > -1
+      ) {
         obj.values[obj.values.length - 1] = {
           key: lastToken.value,
-          value: null,
+          value: undefined, // No value
           index: lastToken.index,
           row: lastToken.row,
-          col:  lastToken.col
+          col: lastToken.col
         }
-
       } else {
         // TODO:Handle null and other types
         // Throw Invalid key error!
@@ -96,35 +132,29 @@ export default class ASTParser {
     // New Token
     else if (token.value === COMMA) {
       // Empty Token Found
-      if (this.lastToken !== null && this.lastToken.value === ',') {
-        this._addValue(null, token.index, token.row, token.col)
+      if (this._lastToken && this._lastToken.value === ',') {
+        this._addValue(undefined, token.index, token.row, token.col)
       }
     }
 
     // Close object and close array
     else if (token.value === CURLY_CLOSED || token.value === SQUARE_CLOSED) {
       this._pop(token.value === CURLY_CLOSED ? 'object' : 'array', token)
-    }
-
-    else if (token.value === TILDE) {
+    } else if (token.value === TILDE) {
       // The ~ sign can occur in any section (header/data) first.
       // If it is a new tree, or header/schema is already been established,
       // set the collection mode to true.
-      if (this.stack.length === 0) {
+      if (this._stack.length === 0) {
         this._isCollection = true
       }
       // When the TILDE is encountered second time check collection mode is on,
       // if not, throw an error
-      else if (!this._isCollection && !this.tree.data) {
+      else if (!this._isCollection && !this._tree.data) {
         // TODO: Throw an error here
-        console.log(this.tree)
-        console.log(this.stack)
         throw new InternetObjectSyntaxError(ErrorCodes.invalidCollection, undefined, token)
-      }
-      else {
+      } else {
         this._processObject()
       }
-
     }
 
     // Ready for schema generation
@@ -132,76 +162,79 @@ export default class ASTParser {
       // Header is closed when, datasep is found. After the header
       // is closed, throw an error when another datasep is sent!
       if (!this._isHeaderOpen) {
-        throw new InternetObjectSyntaxError(ErrorCodes.multipleHeaders,"Multiple headers are not allowed.", token)
+        throw new InternetObjectSyntaxError(
+          ErrorCodes.multipleHeaders,
+          'Multiple headers are not allowed.',
+          token
+        )
       }
       this._processObject()
       this._isHeaderOpen = false
       this._isCollection = false
-      this.stack.length = 0
+      this._stack.length = 0
       // this._addToHeader()
-
-    }
-    else {
+    } else {
       this._addValue(token, token.index, token.row, token.col)
     }
 
-    this.lastToken = token
+    this._lastToken = token
   }
 
   private _processObject = () => {
     const isCollection = this._isCollection
     const isHeaderOpen = this._isHeaderOpen
-    const data = this.stack[0]
+    const data = this._stack[0]
 
     // Nothing to process
-    if (this.stack.length === 0) {
+    if (this._stack.length === 0) {
       // TODO: Handle this case
     }
 
     // When there are open objects and arrays, stack will contain
     // more than one items.
-    if (this.stack.length > 1) {
+    if (this._stack.length > 1) {
       // TODO: Throw a proper error here
-      throw new InternetObjectSyntaxError(ErrorCodes.openBracket, "Expecting bracket to be closed", this.lastToken || undefined)
+      throw new InternetObjectSyntaxError(
+        ErrorCodes.openBracket,
+        'Expecting bracket to be closed',
+        this._lastToken || undefined
+      )
     }
 
     // Process header section
     if (isHeaderOpen) {
       if (isCollection) {
-        if (!this.tree.header) {
-          this.tree.header = {
-            type: "collection",
+        if (!this._tree.header) {
+          this._tree.header = {
+            type: 'collection',
             values: [data]
           }
+        } else {
+          // console.log("2.", data)
+          this._tree.header.values.push(data)
         }
-        else {
-          this.tree.header.values.push(data)
-        }
+      } else {
+        this._tree.header = data
       }
-      else {
-        this.tree.header = data
-      }
-    }
-    else {
+    } else {
       // Process data section
       if (isCollection) {
-        if (!this.tree.data) {
-          this.tree.data = {
-            type: "collection",
+        if (!this._tree.data) {
+          this._tree.data = {
+            type: 'collection',
             values: [data]
           }
+        } else {
+          // console.log("3.", data)
+          this._tree.data.values.push(data)
         }
-        else {
-          this.tree.data.values.push(data)
-        }
-      }
-      else {
-        this.tree.data = data
+      } else {
+        this._tree.data = data
       }
     }
 
     // Reset stack
-    this.stack.length = 0
+    this._stack.length = 0
   }
 
   private _finalize = () => {
@@ -210,120 +243,94 @@ export default class ASTParser {
     this._processObject()
 
     // TODO: Consider the scenario when more than 1 item exists in the stack!
-    if (this.stack.length > 1) {
-      console.warn(this._text, this.stack)
-      console.warn("Invalid Stack")
+    if (this._stack.length > 1) {
+      console.warn('Invalid Stack', this._stack)
     }
 
     if (this._isHeaderOpen) {
-      this.tree.data = this.tree.header
-      delete this.tree.header
+      this._tree.data = this._tree.header
+      delete this._tree.header
 
       if (this._tokenizer.length === 1) {
-        this.tree.data.type = "scalar"
+        this._tree.data.type = 'scalar'
       }
     }
 
-    if (this._schemaOnly) {
-      this.tree.header = this.tree.data
-      this.tree.data = null
+    if (this._isSchema) {
+      this._tree.header = this._tree.data
+      this._tree.data = undefined // TODO: Replace this with `undefined`
     }
-
-    // print("TOKENIZER", this._tokenizer)
-    // print("TREE", this.stack)
-
-    return
-
-    // // If no data found just return
-    // if (this.stack.length === 0) return
-
-    // let data = this.stack[0]
-
-    // const tokenCount = this._tokenizer.length
-
-    // if (this.tree.data === undefined) {
-    //   if (this._schemaOnly) {
-    //     this.tree.header = data
-    //     // TODO: Handle the case when compiling for schema
-    //     // and token count is 1
-    //     console.warn("Compiling for schema while token count is 1")
-    //   }
-    //   else {
-    //     if (tokenCount === 1) {
-    //       data.type = "scalar"
-    //     }
-    //     this.tree.data = data
-    //   }
-    // }
   }
 
-  private _getOrCreateObject = (index:number, row:number, col:number) => {
-    if (this.stack.length === 0) {
-      this.stack.push({
+  private _getOrCreateObject = (index: number, row: number, col: number) => {
+    if (this._stack.length === 0) {
+      this._stack.push({
         type: 'object',
         values: [],
-        index, row, col
+        index,
+        row,
+        col
       })
     }
 
-    let obj = this.stack[this.stack.length - 1]
+    let obj = this._stack[this._stack.length - 1]
     return obj
   }
 
   private _getObject = () => {
-    // if (this.stack.length === 0) {
-    //   this.stack.push({
+    // TODO: Verify and remove the commented code!
+    // if (this._stack.length === 0) {
+    //   this._stack.push({
     //     type: 'object',
     //     values: []
     //   })
     // }
 
-    let obj = this.stack[this.stack.length - 1]
+    let obj = this._stack[this._stack.length - 1]
     return obj
   }
 
-  private _addValue = (value: any, index:number, row:number, col:number) => {
-
+  private _addValue = (value: any, index: number, row: number, col: number) => {
     let obj = this._getOrCreateObject(index, row, col)
 
     // If last token is : then
-    if (this.lastToken !== null && this.lastToken.value === ':') {
+    if (this._lastToken && this._lastToken.value === ':') {
       let lastVal = obj.values[obj.values.length - 1]
 
       // When the lastVal is a KeyVal, set the current token as
       // the value of lastVal
       if (lastVal && isKeyVal(lastVal)) {
         lastVal.value = value
-      }
-      else {
+      } else {
         // TODO: Verify this case!
         console.warn('Verify this case!')
       }
-    }
-    else {
+    } else {
       this._checkValueSlot(value)
       obj.values.push(value)
     }
   }
 
-  private _checkValueSlot(value:any) {
-    const token:any = this.lastToken
-    if (token === null) return
-    if (value === null) return
+  private _checkValueSlot(value: any) {
+    const token: any = this._lastToken
+    if (token === undefined) return
+    if (value === undefined) return // TODO: Check this, replaced null with undefined
 
-    if (
-      [",", ":", "~", "{", "[", "---"].indexOf(token.value) === -1
-    ) {
+    if ([',', ':', '~', '{', '[', '---'].indexOf(token.value) === -1) {
       // TODO: Provide better error
-      throw new InternetObjectSyntaxError(ErrorCodes.expectingSeparator, `Error while parsing ${value.token}`, value)
+      throw new InternetObjectSyntaxError(
+        ErrorCodes.expectingSeparator,
+        `Error while parsing ${value.token}`,
+        value
+      )
     }
   }
 
-  private _push = (type = 'object', index:number, row:number, col:number, values = []) => {
+  private _push = (type = 'object', index: number, row: number, col: number, values = []) => {
     let object = this._getOrCreateObject(index, row, col)
     let newObject = { type, values, index, row, col }
     this._addValue(newObject, index, row, col)
-    this.stack.push(newObject)
+    this._stack.push(newObject)
   }
 
   private _pop = (type: string, token: Token) => {
@@ -332,10 +339,10 @@ export default class ASTParser {
     if (obj.type !== type) {
       const message = `Expecting "${obj.type === 'object' ? '}' : ']'}" but found "${
         obj.type === 'object' ? ']' : '}'
-        }"`
+      }"`
       this.status = ERROR
       throw new InternetObjectSyntaxError(ErrorCodes.invalidBracket, message, token)
     }
-    this.stack.pop()
+    this._stack.pop()
   }
 }
