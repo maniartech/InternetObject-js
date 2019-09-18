661c7800afae754ef6067f25b0d3036c6095404a
Aamir Maniar <aamironline@gmail.com>
2019-09-18 08:54:39 +0530

    Added valueRequired and nullNotAllowed errorCodes

--- src/errors/io-error-codes.ts
+++ src/errors/io-error-codes.ts
@@ -1,46 +1,46 @@
 enum ErrorCodes {
-
   // General
-  invalidType               = "invalid-type",
-  tokenNotReady             = "token-not-ready",
-  invalidValue              = "invalid-value",
+  invalidType = 'invalid-type',
+  tokenNotReady = 'token-not-ready',
+  invalidValue = 'invalid-value',
+  valueRequired = 'value-required',
+  nullNotAllowed = 'null-not-allowed',
 
   // Parser Errors (Header)
-  multipleHeaders           = "multiple-headers-found",
-  invalidCollection         = "invalid-collection",
-  invlidHeader              = "invalid-header",
-  invalidHeaderItem         = "invalid-header-item",
-  expectingSeparator        = "expecting-a-separator",
-  openBracket               = "open-bracket",
-  invalidBracket            = "invalid-bracket",
-  incompleteEscapeSequence  = "incomplete-escape-sequence",
+  multipleHeaders = 'multiple-headers-found',
+  invalidCollection = 'invalid-collection',
+  invlidHeader = 'invalid-header',
+  invalidHeaderItem = 'invalid-header-item',
+  expectingSeparator = 'expecting-a-separator',
+  openBracket = 'open-bracket',
+  invalidBracket = 'invalid-bracket',
+  incompleteEscapeSequence = 'incomplete-escape-sequence',
 
   // Parser Errors (Schema)
-  invalidSchema             = "invalid-schema",
+  invalidSchema = 'invalid-schema',
 
   // -- string, number, date, datetime, time
-  valueNotInChoice          = "value-not-in-choice",
-  invalidChoice             = "invalid-choice",
+  valueNotInChoice = 'value-not-in-choice',
+  invalidChoice = 'invalid-choice',
 
   // -- string, array
-  invalidMinLength          = "invalid-min-length",
-  invalidMaxLength          = "invalid-max-length",
-  invalidChar               = "invalid-char",
-  invalidEmail              = "invalid-email",
-  invalidUrl                = "invalid-url",
+  invalidMinLength = 'invalid-min-length',
+  invalidMaxLength = 'invalid-max-length',
+  invalidChar = 'invalid-char',
+  invalidEmail = 'invalid-email',
+  invalidUrl = 'invalid-url',
 
   // Number
-  notANumber                = "not-a-number",
-  notAnInteger              = "not-an-integer",
-  outOfRange                = "out-of-range",
-  invalidMinValue           = "invalid-min-value",
-  invalidMaxValue           = "invalid-max-value",
+  notANumber = 'not-a-number',
+  notAnInteger = 'not-an-integer',
+  outOfRange = 'out-of-range',
+  invalidMinValue = 'invalid-min-value',
+  invalidMaxValue = 'invalid-max-value',
 
   // String
-  notAString                = "not-a-string",
+  notAString = 'not-a-string'
 
   // Object
-
 
   // Array
 }
