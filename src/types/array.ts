f8a00d8ce91f24b26dac18fe78fc312a75cc2302
Aamir Maniar <aamironline@gmail.com>
2019-09-20 23:37:27 +0530

    Code cleanup

--- src/types/array.ts
+++ src/types/array.ts
@@ -1,24 +1,17 @@
-import ErrorCodes from '../errors/io-error-codes'
-import { ParserTreeValue, Node } from '../parser/index'
-import { Token } from '../parser'
-import { isParserTree, isKeyVal, isString } from '../utils/is'
 import MemberDef from './memberdef'
 import TypeDef from './typedef'
-import { TypedefRegistry } from './typedef-registry'
-import { doCommonTypeCheck } from './utils'
-import { isArray } from 'util'
-import { isToken } from '../../../src-with-defs/utils/is'
+import ErrorCodes from '../errors/io-error-codes'
 
-// age?: { number, true, 10, min:10, max:20}
+import { isArray } from 'util'
+import { Token } from '../parser'
+import { isParserTree } from '../utils/is'
+import { doCommonTypeCheck } from './utils'
+import { TypedefRegistry } from './typedef-registry'
+import { Node, ParserTreeValue } from '../parser/index'
+import { InternetObjectValidationError, ErrorArgs } from '../errors/io-error'
 
 /**
- * Represents the `array`, performs following validations.
- * - Value is an array
- * - Value is optional
- * - Value is nullable
- * - array is <= schema.maxLength
- * - array is >= schema.minLength
- * - Value is in choices
+ * Represents an `array`.
  */
 class ArrayDef implements TypeDef {
   private _keys: any = null
@@ -34,52 +27,36 @@ class ArrayDef implements TypeDef {
       return _process('load', memberDef, undefined, data)
     }
 
-    throw new Error('invalid-value')
+    // TODO: check this case
+    console.assert(false, 'Check this case!')
   }
 
   public load = (data: any, memberDef: MemberDef): any => {
     const value = data === undefined ? undefined : data
     return _process('load', memberDef, value, data)
-
-    // const validatedData = doCommonTypeCheck(memberDef)
-    // if (validatedData !== data) return validatedData
-
-    // if (!isArray(data)) throw new Error("invalid-value")
-
-    // const schema = memberDef.schema
-
-    // let typeDef:TypeDef | undefined
-
-    // if (schema.type) {
-    //   typeDef = TypedefRegistry.get(schema.type)
-    // }
-    // else {
-    //   console.assert(false, "Invalid Case: Array schema must have a type attribute!")
-    //   throw new Error("Verify this case!")
-    // }
-
-    // const array:any = []
-
-    // data.forEach((item:any) => {
-    //   if(typeDef !== undefined) {
-    //     const value = typeDef.load(item, schema)
-    //     array.push(value)
-    //   }
-    //   else {
-    //     // TODO: Improve this error
-    //     throw ErrorCodes.invalidType
-    //   }
-    // })
-
-    // return array
   }
 }
 
+// Processes an array and performs following validations
+// - Value is an array
+// - Value is optional
+// - Value is nullable
+// - array is <= schema.maxLength
+// - array is >= schema.minLength
+// - Value is in choices
 function _process(processingFnName: string, memberDef: MemberDef, value: any, node?: Node) {
   const validatedData = doCommonTypeCheck(memberDef, value, node)
-  if (validatedData !== value) return validatedData
+  if (validatedData !== value || validatedData === undefined) return validatedData
 
   if (!isArray(value)) throw new Error('invalid-value')
+
+  if (memberDef.minLength && value.length < memberDef.minLength) {
+    throw new InternetObjectValidationError(..._invlalidMinLength(memberDef, value, node))
+  }
+
+  if (memberDef.maxLength && value.length < memberDef.maxLength) {
+    throw new InternetObjectValidationError(..._invlalidMaxLength(memberDef, value, node))
+  }
 
   const schema = memberDef.schema
 
@@ -107,27 +84,27 @@ function _process(processingFnName: string, memberDef: MemberDef, value: any, no
   return array
 }
 
-function _invlalidChoice(key: string, token: Token, min: number) {
+function _invlalidArray(memberDef: MemberDef, value: any, node?: Node): ErrorArgs {
   return [
-    ErrorCodes.invalidMinValue,
-    `The "${key}" must be greater than or equal to ${min}, Currently it is "${token.value}".`,
-    token
+    ErrorCodes.invalidArray,
+    `For "${memberDef.path}" expecting and array, however found this "${typeof value}".`,
+    node
   ]
 }
 
-function _invlalidMinLength(key: string, token: Token, min: number) {
+function _invlalidMinLength(memberDef: MemberDef, value: any, node?: Node): ErrorArgs {
   return [
-    ErrorCodes.invalidMinValue,
-    `The "${key}" must be greater than or equal to ${min}, Currently it is "${token.value}".`,
-    token
+    ErrorCodes.invalidMinLength,
+    `The length of the "${memberDef.path}" must be greater than or equal to ${memberDef.minLength}, currently it is "${value.length}".`,
+    node
   ]
 }
 
-function _invlalidMaxLength(key: string, token: Token, max: number) {
+function _invlalidMaxLength(memberDef: MemberDef, value: any, node?: Node): ErrorArgs {
   return [
-    ErrorCodes.invalidMaxValue,
-    `The "${key}" must be less than or equal to ${max}, Currently it is "${token.value}".`,
-    token
+    ErrorCodes.invalidMinLength,
+    `The length of the "${memberDef.path}" must be less than or equal to ${memberDef.maxLength}, currently it is "${value.length}".`,
+    node
   ]
 }
 
