dcea6deb782c4f62a9c255e6c343afc0293334f8
Aamir Maniar <aamironline@gmail.com>
2019-09-20 23:47:10 +0530

    Code cleanup

--- src/types/memberdef.ts
+++ src/types/memberdef.ts
@@ -1,12 +1,14 @@
-
-
+/**
+ * Defines the MemberDef object!
+ * @internal
+ */
 export default interface MemberDef {
-  type:string,
-  path:string,
-  optional?:boolean,
-  null?:any,
-  default?:any,
-  choices?:any[],
-  schema?:any,
-  [index:string]:any
+  type: string
+  path: string
+  optional?: boolean
+  null?: any
+  default?: any
+  choices?: any[]
+  schema?: any // If type is an object or array, associated schema
+  [index: string]: any // Any other values
 }
