f62d6111162ad78576eea3aa70983d276540ff15
Aamir Maniar <aamir.maniar@maniartech.com>
2020-05-04 18:18:44 +0530

    yarn does not require -- for passing the parameters to the host command

--- test/trial.test.ts
+++ test/trial.test.ts
@@ -6,9 +6,8 @@ import IO from '../src/'
  * To run the test trialpad, run following commands.
  *
  * npm run test -- trial
- * yarn test -- trial
+ * yarn test trial
  */
-
 describe('Test Pad', () => {
   it('helps you test anything quickly', () => {
     // Quickly test anything here, just don't push changes to the server!
