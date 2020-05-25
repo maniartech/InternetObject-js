06a3758de5ca9a90ca0e21dbafdacb3bd33926d1
Aamir Maniar <aamironline@gmail.com>
2020-05-25 22:50:10 +0530

    Tested - array and objects dont count last empty value

--- test/parser/data-parser.test.ts
+++ test/parser/data-parser.test.ts
@@ -2,6 +2,7 @@ import 'jest'
 
 import ASTParser from '../../src/parser/ast-parser'
 import ErrorCodes from '../../src/errors/io-error-codes'
+import { VAL } from '../../src/parser/constants'
 
 describe('Data Parser Tests', () => {
   it('handles key errors', () => {
@@ -11,5 +12,67 @@ describe('Data Parser Tests', () => {
       `)
       parser.parse()
     }).toThrowError(ErrorCodes.unexpectedColon)
+  })
+
+  it('handles empty values in object', () => {
+    // {,,,}  An object with four undefined values
+    let parser = new ASTParser('{,,,}')
+    parser.parse()
+    expect(parser.data.values.length).toBe(4)
+    expect(parser.data.values[0]).toBeUndefined()
+    expect(parser.data.values[1]).toBeUndefined()
+    expect(parser.data.values[2]).toBeUndefined()
+    expect(parser.data.values[3]).toBeUndefined()
+
+    // { , , , }  An object with four undefined values (spaced)
+    parser = new ASTParser('{ , , , }')
+    parser.parse()
+    expect(parser.data.values.length).toBe(4)
+    expect(parser.data.values[0]).toBeUndefined()
+    expect(parser.data.values[1]).toBeUndefined()
+    expect(parser.data.values[2]).toBeUndefined()
+    expect(parser.data.values[3]).toBeUndefined()
+
+    // {a,b,c,,,}    An object with four undefined values
+    parser = new ASTParser('{a,b,c,,,}')
+    parser.parse()
+    expect(parser.data.values.length).toBe(6)
+    expect(parser.data.values[0].value).toBe('a')
+    expect(parser.data.values[1].value).toBe('b')
+    expect(parser.data.values[2].value).toBe('c')
+    expect(parser.data.values[3]).toBeUndefined()
+    expect(parser.data.values[4]).toBeUndefined()
+    expect(parser.data.values[5]).toBeUndefined()
+  })
+
+  it('handles empty values in array', () => {
+    // [,,,]    An array with four undefined values
+    let parser = new ASTParser('[,,,]')
+    parser.parse()
+    expect(parser.data.values.length).toBe(4)
+    expect(parser.data.values[0]).toBeUndefined()
+    expect(parser.data.values[1]).toBeUndefined()
+    expect(parser.data.values[2]).toBeUndefined()
+    expect(parser.data.values[3]).toBeUndefined()
+
+    // [ , , , ]    An array with four undefined values (spaced)
+    parser = new ASTParser('[ , , , ]')
+    parser.parse()
+    expect(parser.data.values.length).toBe(4)
+    expect(parser.data.values[0]).toBeUndefined()
+    expect(parser.data.values[1]).toBeUndefined()
+    expect(parser.data.values[2]).toBeUndefined()
+    expect(parser.data.values[3]).toBeUndefined()
+
+    // [a,b,c,,,]    An array with four undefined values
+    parser = new ASTParser('[a,b,c,,,]')
+    parser.parse()
+    expect(parser.data.values.length).toBe(6)
+    expect(parser.data.values[0].value).toBe('a')
+    expect(parser.data.values[1].value).toBe('b')
+    expect(parser.data.values[2].value).toBe('c')
+    expect(parser.data.values[3]).toBeUndefined()
+    expect(parser.data.values[4]).toBeUndefined()
+    expect(parser.data.values[5]).toBeUndefined()
   })
 })
