11708c7f24be3dbaf23ece8a1a3413e3d0cbcdb1
Aamir Maniar <aamironline@gmail.com>
2020-05-25 19:32:11 +0530

    Tested unexpected colon error

--- test/parser/data-parser.test.ts
+++ test/parser/data-parser.test.ts
@@ -1,58 +1,15 @@
 import 'jest'
-import InternetObject from '../../src'
+
+import ASTParser from '../../src/parser/ast-parser'
+import ErrorCodes from '../../src/errors/io-error-codes'
 
 describe('Data Parser Tests', () => {
-  it('Blank string', () => {
-    expect(new InternetObject('').data).toBeUndefined()
-    expect(new InternetObject('         ').data).toBeUndefined()
-    expect(new InternetObject(String.raw`    \n,\t     `).data[0]).toBe('\\n')
-    expect(new InternetObject(String.raw`    \n,\t     `).data[1]).toBe('\\t')
-    expect(new InternetObject(String.raw`    \n \t,     `).data[0]).toBe('\\n \\t')
-    // expect(() => {
-    //   return new InternetObject(String.raw`  \n  \t `)
-    // }).toThrowError()
-  })
-
-  it('All optional with no value', () => {
-    expect(new InternetObject('"   "').data).toBe('   ')
-    expect(new InternetObject('""').data).toBe('')
-    expect(new InternetObject(String.raw`"  \n  \t "`).data).toBe('  \n  \t ')
-  })
-
-  it('handles empty ending commas', () => {
-    expect(new InternetObject(String.raw`  \n \t , `).data[0]).toBe('\\n \\t')
-    expect(new InternetObject(String.raw`  \n \t , `).data[1]).toBe(undefined)
-    expect(new InternetObject(String.raw`  \n \t ,,, `).data[0]).toBe('\\n \\t')
-    expect(Object.keys(new InternetObject(String.raw`  \n \t , `).data).join(',')).toBe('0,1')
-    expect(Object.keys(new InternetObject(String.raw`  \n \t , , , `).data).join(',')).toBe(
-      '0,1,2,3'
-    )
-    expect(
-      Object.keys(
-        new InternetObject(String.raw`
-    ~ ,,,`).data[0]
-      ).join(',')
-    ).toBe('0,1,2,3')
-
-    expect(
-      new InternetObject(String.raw`
-    ~ 
-    `).data
-    ).toBeUndefined()
-
-    expect(
-      Object.keys(
-        new InternetObject(String.raw`
-    ~ ,,,`).data[0]
-      ).join(',')
-    ).toBe('0,1,2,3')
+  it('handles key errors', () => {
+    expect(() => {
+      const parser = new ASTParser(String.raw`
+        name: John Doe: age: 40
+      `)
+      parser.parse()
+    }).toThrowError(ErrorCodes.unexpectedColon)
   })
 })
-
-// {,,,}    An object with four undefined values
-// [,,,]    An array with four undefined items
-// --- ,,,  A data object with four undefined values
-// ,,,      An object with four undefined values
-// k:,
-// ~,
-// ~
