2fca91007aec1623e3260a5cf4045e73187db6c9
Aamir Maniar <aamironline@gmail.com>
2023-10-17 00:19:53 +0530

    Setup parser

--- src/parser/nodes/tokens.ts
+++ src/parser/nodes/tokens.ts
@@ -4,7 +4,15 @@ import Node from "./nodes";
 
 class TokenNode extends Token implements Node {
   constructor (token: Token) {
-    super(token.pos, token.row, token.col, token.token, token.value, token.type, token.subType);
+    super();
+
+    this.pos = token.pos;
+    this.row = token.row;
+    this.col = token.col;
+    this.token = token.token;
+    this.value = token.value;
+    this.type = token.type;
+    this.subType = token.subType;
   }
 
   toValue: () => any = () => {
