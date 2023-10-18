09f97fc2fea2790ef861f21d0d5621d30fc93517
Aamir Maniar <aamironline@gmail.com>
2023-10-18 18:45:21 +0530

    Document parsing WIP

---
+++ src/parser/io.ts
@@ -0,0 +1,18 @@
+import Parser from "./ast-parser";
+import Document from '../core/document';
+import Tokenizer from "../tokenizer";
+
+
+function io(strings: TemplateStringsArray, ...values: any[]): Document {
+  const input = strings.raw.join('');
+  const tokenizer = new Tokenizer(input);
+  const tokens = tokenizer.tokenize();
+
+  const parser = new Parser(tokens);
+  const result = parser.parse();
+
+  console.log(result);
+  return new Document()
+}
+
+export default io;
---
+++ src/parser/parser-options.ts
@@ -0,0 +1,7 @@
+
+
+class ParserOptions {
+  strict?: boolean = false;
+}
+
+export default ParserOptions;
