7a05ecc9987dc235377995dff0f05ea92d01094a
Aamir Maniar <aamironline@gmail.com>
2023-10-24 15:08:00 +0530

    Schema generation

--- tests/trial.test.ts
+++ tests/trial.test.ts
@@ -2,7 +2,9 @@ import Tokenizer from "../src/tokenizer";
 
 describe('Trial', () => {
   it('should try wip tasks', () => {
-    const input = String.raw`d"2020-01-01"`
+    const input = String.raw`
+    testing
+    `
     const tokenizer = new Tokenizer(input);
     const tokens = tokenizer.tokenize();
     console.log(tokens);
