170cbdc9f07a701d9de8ec0e674fd297bd8d03ba
Aamir Maniar <aamironline@gmail.com>
2023-10-25 13:37:32 +0530

    Schema parsing WIP

--- src/core/definitions.ts
+++ src/core/definitions.ts
@@ -15,6 +15,21 @@ class Definitions {
     return Object.keys(this._definitions);
   }
 
+  /**
+   * Gets the varialbe value. This function is intend to be used internally
+   * for quickly fetching the variable value, hence it accepts any key to keep the
+   * consumer code free from type checking. The function validate the key and
+   * returns the associated value, if avialable. Otherwise returns undefined.
+   * @param key {any} The varialbe key starting with $
+   * @returns The value associated with the variable
+   */
+  public getV(key: any) {
+    if (typeof key === 'string' && key.startsWith('$') && key.length > 1) {
+      return this._definitions[key.substring(1)];
+    }
+  }
+
+
 }
 
 const proxy = {
