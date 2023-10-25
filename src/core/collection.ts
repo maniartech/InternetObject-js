34ce7240d85b584e93d3bdb1937bdf1b7a599f17
Aamir Maniar <aamironline@gmail.com>
2023-10-26 00:02:23 +0530

    Schema processing WIP

--- src/core/collection.ts
+++ src/core/collection.ts
@@ -1,7 +1,31 @@
+import InternetObject from './internet-object';
 
+class Collection<T = InternetObject> {
+  _items: T[] | null = null;
 
-class Collection<T> {
+  public push(item: T): Collection<T> {
+    if (!this._items) {
+      this._items = [];
+    }
 
+    this._items.push(item);
+    return this;
+  }
+
+  public get length(): number {
+    return this._items?.length || 0;
+  }
+
+  public toObject(): any {
+    return this._items?.map((item) => {
+      if (item instanceof InternetObject) {
+        return item.toObject();
+      }
+
+      // Unsupported type
+      throw new Error('Invalid item type.');
+    });
+  }
 }
 
 export default Collection;
--- src/core/document.ts
+++ src/core/document.ts
@@ -1,22 +1,50 @@
 import Header from "./header";
+import Section from "./section";
 import SectionCollection from "./section-collection";
 
 
 class Document {
   private _header: Header;
-  private _sections: SectionCollection;
+  private _sections: SectionCollection | null;
 
-  constructor(header: Header, sections: SectionCollection) {
+  constructor(header: Header, sections: SectionCollection | null) {
     this._header = header
     this._sections = sections
   }
 
-  public get header(): Header {
+  public get header(): Header{
     return this._header;
   }
 
-  public get sections(): SectionCollection{
+  public get sections(): SectionCollection | null{
     return this._sections;
+  }
+
+  /**
+   * Conerts the data sections into a JavaScript object.
+   */
+  public toObject(): any {
+    const sectionsLen = this._sections?.length || 0
+
+
+    // Only one section
+    if (sectionsLen === 1) {
+      const section = this._sections?.get(0) as Section
+      return section.toObject()
+    }
+
+    // Multiple sections
+    if (sectionsLen > 1) {
+      const result: any = {}
+      for (let i=0; i<sectionsLen; i++) {
+        const section = this._sections?.get(i) as Section
+        result[section.name as string] = section.toObject()
+      }
+      return result
+    }
+
+    // No sections
+    return null
   }
 }
 
