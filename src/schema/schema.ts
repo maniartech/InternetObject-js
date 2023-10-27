c592bce0a3b0a50eb97eda929062b62ba5f17aae
Aamir Maniar <aamironline@gmail.com>
2023-10-27 10:16:54 +0530

    Schema processing WIP

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -1,13 +1,4 @@
-import { InternetObjectSyntaxError } from "../errors/io-error";
-import ErrorCodes from "../errors/io-error-codes";
-import { MemberNode, Node, ObjectNode, TokenNode } from "../parser/nodes";
-import TokenType from "../tokenizer/token-types";
-import Token from "../tokenizer/tokens";
 import MemberDef from "../types/memberdef";
-import ObjectDef from "../types/object";
-import TypeDef from "../types/typedef";
-
-
 
 export default class Schema {
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,8 @@
 import AnyDef from './any'
 // import ArrayDef from './array'
 import BooleanDef from './boolean'
-// import NumberDef from './number'
-// import ObjectDef from './object'
+import NumberDef from './number'
+import ObjectDef from './object'
 import StringDef from './string'
 // import DateTimeDef from './datetime'
 
@@ -21,18 +21,18 @@ export default function registerTypes() {
   TypedefRegistry.register('email', new StringDef('email'))
   TypedefRegistry.register('url', new StringDef('url'))
 
-  // TypedefRegistry.register('number', new NumberDef('number'))
-  // TypedefRegistry.register('byte', new NumberDef('byte'))
-  // TypedefRegistry.register('int16', new NumberDef('int16'))
-  // TypedefRegistry.register('int32', new NumberDef('int32'))
-  // TypedefRegistry.register('int', new NumberDef('int'))
+  TypedefRegistry.register('number', new NumberDef('number'))
+  TypedefRegistry.register('byte', new NumberDef('byte'))
+  TypedefRegistry.register('int16', new NumberDef('int16'))
+  TypedefRegistry.register('int32', new NumberDef('int32'))
+  TypedefRegistry.register('int', new NumberDef('int'))
 
   // TypedefRegistry.register('datetime', new DateTimeDef('datetime'))
   // TypedefRegistry.register('date', new DateTimeDef('date'))
   // TypedefRegistry.register('time', new DateTimeDef('time'))
 
   // TypedefRegistry.register('array', new ArrayDef())
-  // TypedefRegistry.register('object', new ObjectDef())
+  TypedefRegistry.register('object', new ObjectDef())
 
   // TODO: Include Date and DateTime Types
 
