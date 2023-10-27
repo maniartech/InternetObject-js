f856f7f2d48231280cdf9be2461fe310f482533d
Aamir Maniar <aamironline@gmail.com>
2023-10-27 10:16:40 +0530

    Schema processing WIP

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,25 +1,23 @@
 
-import Document from "../core/document";
-import Tokenizer from "../tokenizer";
-import ParserOptions from "./parser-options";
-import ASTParser from './ast-parser';
-import Header from "../core/header";
-import { compileObject } from "../schema";
-import Schema from "../schema/schema";
-import processSchema from "../schema/processor";
-import { ObjectNode } from "./nodes";
-import Section from "../core/section";
-import SectionCollection from "../core/section-collection";
-
+import Document               from '../core/document';
+import Header                 from '../core/header';
+import Section                from '../core/section';
+import SectionCollection      from '../core/section-collection';
+import {
+       compileObject        } from '../schema';
+import processSchema          from '../schema/processor';
+import Tokenizer              from '../tokenizer';
+import ASTParser              from './ast-parser';
+import {
+       ObjectNode           } from './nodes';
+import ParserOptions          from './parser-options';
 
 export default function parse(source: string, o: ParserOptions = {}): Document {
   const tokenizer = new Tokenizer(source);
-  const tokens = tokenizer.tokenize();
-
-  const parser = new ASTParser(tokens);
-  const docNode = parser.parse();
-
-  const doc = new Document(new Header(), new SectionCollection())
+  const tokens    = tokenizer.tokenize();
+  const parser    = new ASTParser(tokens);
+  const doc       = new Document(new Header(), new SectionCollection())
+  const docNode   = parser.parse();
 
   // documentNode.header
   if (docNode.header) {
@@ -35,11 +33,6 @@ export default function parse(source: string, o: ParserOptions = {}): Document {
       const result = processSchema(sectionNode.child as ObjectNode, doc.header.schema)
       doc.sections?.push(new Section(result, sectionNode.name))
     }
-
-    // const rootObj = docNode.children[0].child
-    // const result = processSchema(rootObj as ObjectNode, doc.header.schema)
-
-    // doc.sections?.push(new Section(result))
   }
 
   return doc;
