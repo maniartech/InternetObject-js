45fec028ebcde2911843976b3e472c66db0cb194
Aamir Maniar <aamironline@gmail.com>
2023-11-20 08:26:21 +0530

    ParserOptions WIP

--- src/parser/parser-options.ts
+++ src/parser/parser-options.ts
@@ -1,7 +1,28 @@
 
 
 class ParserOptions {
-  strict?: boolean = false;
+  readonly continueOnError?: boolean
+
+  readonly allowEmptyRecords?: boolean
+
+  readonly numberOfSections?: number
+
+  readonly dataOnly?: boolean
+
+  readonly headerOnly?: boolean
+
+  readonly skipEmptyLines?: boolean
+
+  readonly allowVariables?: boolean
+
+  constructor(o: ParserOptions = {}) {
+    this.continueOnError    = o.continueOnError || false;
+    this.allowEmptyRecords  = o.allowEmptyRecords || false;
+    this.numberOfSections   = o.numberOfSections || 0;
+    this.dataOnly           = o.dataOnly || false;
+    this.skipEmptyLines     = o.skipEmptyLines || false;
+    this.allowVariables     = o.allowVariables || false;
+  }
 }
 
 export default ParserOptions;
