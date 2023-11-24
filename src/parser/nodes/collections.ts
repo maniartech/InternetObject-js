5ba6e54222b889704eca5872462d2f9995b4d2bd
Aamir Maniar <aamironline@gmail.com>
2023-11-24 15:17:35 +0530

    CollectionNode.toValue returns the Collection

--- src/parser/nodes/collections.ts
+++ src/parser/nodes/collections.ts
@@ -1,17 +1,15 @@
+import Collection from "../../core/collection";
 import Definitions from "../../core/definitions";
 import ContainerNode  from "./containers";
 import Node           from "./nodes";
-
-
 
 class CollectionNode extends ContainerNode {
   constructor(children: Array<Node | null> = []) {
     super('collection', children);
   }
 
-
   toValue(defs?: Definitions):any {
-    const value = [];
+    const value = new Collection();
     for (const child of this.children) {
       value.push(child?.toValue(defs));
     }
