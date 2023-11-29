8bd512d5da2b489c21c291d636eea8ec5bc87af6
Aamir Maniar <aamironline@gmail.com>
2023-11-29 09:58:59 +0530

    Error revamp

--- src/errors/io-error.ts
+++ src/errors/io-error.ts
@@ -1,10 +1,11 @@
 import { Node, TokenNode } from '../parser/nodes'
+import Token from '../tokenizer/tokens'
 
 
 /**
  * Represents the base error class in InternetObject.
  */
-export class InternetObjectError extends Error {
+class InternetObjectError extends Error {
   // Due to a bug in TypeScript specifically control the __proto__
   // Ref: https://github.com/Microsoft/TypeScript/issues/13965
   // If not, `instanceof` and `catch` won't work properly
@@ -20,19 +21,19 @@ export class InternetObjectError extends Error {
    * A line number (in the text) where error has occured.
    * Only available with some errors while parsting text data.
    */
-  public lineNumber?: number
+  public row?: number
 
   /**
    * Column number (in the text) where error has occured.
    * Only available with some errors while parsting text data.
    */
-  public columnNumber?: number
+  public col?: number
 
   /**
    * The index (in the text) where error has occured.
    * Only available with some errors while parsting text data.
    */
-  public index?: number
+  public pos?: number
 
   /**
    * Initialize the new instance of `InternetObjectError`.
@@ -42,7 +43,7 @@ export class InternetObjectError extends Error {
    * @param node {Node} The node object, for tracking line and columns. Optional
    * @param ssf {Function} The start statck function, removes the irrelavant frames from the stack trace
    */
-  constructor(errorCode: string, message: string = '', node?: Node, ssf?: any) {
+  constructor(errorCode: string, message: string, token?: Token, ssf?: any) {
     super()
 
     let errorMsg: string = errorCode
@@ -50,13 +51,11 @@ export class InternetObjectError extends Error {
 
     this.name = 'InternetObjectError'
 
-    console.log("ERROR:", node)
-
-    if (node instanceof TokenNode) {
-      this.lineNumber = node.row
-      this.columnNumber = node.col
-      this.index = node.pos
-      errorMsg = `${errorCode} at (${node.row}, ${node.col})`
+    if (token) {
+      this.row = token.row
+      this.col = token.col
+      this.pos = token.pos
+      errorMsg = `${errorCode} at (${token.row}, ${token.col})`
     } else {
       errorMsg = errorCode
     }
@@ -69,44 +68,6 @@ export class InternetObjectError extends Error {
   }
 }
 
-/**
- * Represents the syntax error in InternetObject. When this error is thrwon,
- * it suggests that a syntax in the associated object is is not correct.
- */
-export class InternetObjectSyntaxError extends InternetObjectError {
-  /**
-   * Creates a new `InternetObjectSyntaxError` error.
-   * @param errorCode {string} An errorCode associated with is error
-   * @param message {string} The message which needs to be displayed
-   * @param node {Node} The node object, required while parsing raw internet-object data or schema
-   * @param ssf {Function} The start statck function, removes the irrelavant frames from the stack trace
-   *
-   * @internal
-   */
-  constructor(errorCode: string, message: string = '', node?: Node, ssf?: any) {
-    super(errorCode, message, node, ssf)
-    this.name = 'InternetObject(SyntaxError)'
-  }
-}
+export default InternetObjectError
 
-/**
- * Represents the validation error in InternetObject. This error is thrown when a validation
- * issue is found while validating the internet-object data against the associated schema.
- */
-export class InternetObjectValidationError extends InternetObjectError {
-  /**
-   * Creates a new `InternetObjectValidationError` error.
-   * @param errorCode {string} An errorCode associated with is error
-   * @param message {string} The message which needs to be displayed
-   * @param node {Node} The node object, required while parsing raw internet-object data or schema
-   * @param ssf {Function} The start statck function, removes the irrelavant frames from the stack trace
-   *
-   * @internal
-   */
-  constructor(errorCode: string, message: string = '', node?: Node, ssf?: any) {
-    super(errorCode, message, node, ssf)
-    this.name = 'InternetObject(ValidationError)'
-  }
-}
 
-export type ErrorArgs = [string, string, Node?]
