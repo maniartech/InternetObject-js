9d7834da8c95b4a84e40bacfed1e0052d63f2bd5
Aamir Maniar <aamironline@gmail.com>
2023-11-29 09:58:24 +0530

    Error revamp

--- src/errors/io-error-codes.ts
+++ src/errors/io-error-codes.ts
@@ -1,59 +1,61 @@
 enum ErrorCodes {
   // General
-  invalidType = 'invalid-type',
-  tokenNotReady = 'token-not-ready',
-  invalidValue = 'invalid-value',
-  invalidArray = 'invalid-array',
-  invalidObject = 'invalid-object',
-  valueRequired = 'value-required',
-  nullNotAllowed = 'null-not-allowed',
-  notAllowed = 'not-allowed',
+  // invalidType = 'invalid-type',
+  // tokenNotReady = 'token-not-ready',
+  // invalidValue = 'invalid-value',
+  // invalidArray = 'invalid-array',
+  // invalidObject = 'invalid-object',
+  // valueRequired = 'value-required',
+  // nullNotAllowed = 'null-not-allowed',
+  // notAllowed = 'not-allowed',
 
   // Parser Errors (Header)
-  unexpectedColon = 'unexpected-colon',
-  multipleHeaders = 'multiple-headers-found',
-  invalidCollection = 'invalid-collection',
-  invlidHeader = 'invalid-header',
-  invalidHeaderItem = 'invalid-header-item',
-  expectingSeparator = 'expecting-a-separator',
-  openBracket = 'open-bracket',
-  invalidBracket = 'invalid-bracket',
-  incompleteEscapeSequence = 'incomplete-escape-sequence',
-  positionalMemberAfterKeywordMember = 'positional-member-after-keyword-member',
+  unexpectedToken = 'unexpected-token',
+  // unexpectedColon = 'unexpected-colon',
+  // multipleHeaders = 'multiple-headers-found',
+  // invalidCollection = 'invalid-collection',
+  // invlidHeader = 'invalid-header',
+  // invalidHeaderItem = 'invalid-header-item',
+  // expectingSeparator = 'expecting-a-separator',
+  // openBracket = 'open-bracket',
+  // invalidBracket = 'invalid-bracket',
+  // incompleteEscapeSequence = 'incomplete-escape-sequence',
+  // positionalMemberAfterKeywordMember = 'positional-member-after-keyword-member',
 
   // Parser Errors (Schema)
-  invalidSchema = 'invalid-schema',
-  invalidKey = 'invalid-key',
-  invalidMemberDefCondition = 'invalid-memberdef-condition',
-  invalidMemberDef = 'invalid-memberdef',
+  // invalidSchema = 'invalid-schema',
+  // invalidKey = 'invalid-key',
+  // invalidMemberDefCondition = 'invalid-memberdef-condition',
+  // invalidMemberDef = 'invalid-memberdef',
 
   // String
-  notAString = 'not-a-string',
-  invalidChar = 'invalid-char',
-  invalidEmail = 'invalid-email',
-  invalidUrl = 'invalid-url',
-  stringNotClosed = 'string-not-closed',
+  // notAString = 'not-a-string',
+  // invalidChar = 'invalid-char',
+  // invalidEmail = 'invalid-email',
+  // invalidUrl = 'invalid-url',
+  // stringNotClosed = 'string-not-closed',
+  invalidEscapeSequence = 'invalid-escape-sequence',
 
   // -- string, number, date, datetime, time
-  valueNotInChoice = 'value-not-in-choice',
-  invalidChoice = 'invalid-choice',
+  // valueNotInChoice = 'value-not-in-choice',
+  // invalidChoice = 'invalid-choice',
 
   // -- string, array
-  invalidMinLength = 'invalid-min-length',
-  invalidMaxLength = 'invalid-max-length',
+  // invalidMinLength = 'invalid-min-length',
+  // invalidMaxLength = 'invalid-max-length',
 
   // Number
-  notANumber = 'not-a-number',
-  notAnInteger = 'not-an-integer',
-  outOfRange = 'out-of-range',
-  invalidMinValue = 'invalid-min-value',
-  invalidMaxValue = 'invalid-max-value',
+  // notANumber = 'not-a-number',
+  // notAnInteger = 'not-an-integer',
+  // outOfRange = 'out-of-range',
+  // invalidMinValue = 'invalid-min-value',
+  // invalidMaxValue = 'invalid-max-value',
 
   // Boolean
-  notABool = 'not-a-bool',
+  // notABool = 'not-a-bool',
 
   // DateTime
-  invalidDateTime = 'invalid-datetime'
+  // invalidDateTime = 'invalid-datetime'
 }
 
 export default ErrorCodes
