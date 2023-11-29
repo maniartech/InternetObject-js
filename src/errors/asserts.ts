cd09dc136b96a9c168faa0a08b0a6d4326143f40
Aamir Maniar <aamironline@gmail.com>
2023-11-29 09:58:04 +0530

    Comments updated

--- src/errors/asserts.ts
+++ src/errors/asserts.ts
@@ -1,4 +1,8 @@
 
+/**
+ * Place this function where the code must never reach.
+ * @param x The object which needs to be asserted as never
+ */
 export function assertNever(x: never): never {
   throw new Error("Assert never: " + x);
 }
