dcf146095dd2968fba73d4cf42e8e0ab37af5357
Aamir Maniar <aamironline@gmail.com>
2023-12-01 20:14:17 +0530

    Added some more option

--- src/parser/parser-options.ts
+++ src/parser/parser-options.ts
@@ -1,5 +1,4 @@
 
-
 class ParserOptions {
   readonly continueOnError?: boolean
 
@@ -15,13 +14,26 @@ class ParserOptions {
 
   readonly allowVariables?: boolean
 
+  readonly trueTokens?: string[] // By default, it's ['true', 'T']
+
+  readonly falseTokens?: string[] // By default, it's ['false', 'F']
+
+  // replace \r\n or \r with \n. Default is true
+  readonly normalizeNewline?: boolean
+
   constructor(o: ParserOptions = {}) {
     this.continueOnError    = o.continueOnError || false;
     this.allowEmptyRecords  = o.allowEmptyRecords || false;
     this.numberOfSections   = o.numberOfSections || 0;
+
+    this.headerOnly         = o.headerOnly || false;
     this.dataOnly           = o.dataOnly || false;
+
     this.skipEmptyLines     = o.skipEmptyLines || false;
     this.allowVariables     = o.allowVariables || false;
+
+    this.trueTokens         = o.trueTokens || ['true', 'T'];
+    this.falseTokens        = o.falseTokens || ['false', 'F'];
   }
 }
 
