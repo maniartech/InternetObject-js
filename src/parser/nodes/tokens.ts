c72d3abaf6462970e207ae6d55faa4e5556f2502
Aamir Maniar <aamironline@gmail.com>
2023-12-10 13:22:13 +0530

    Refactor TokenNode class in tokens.ts

--- src/parser/nodes/tokens.ts
+++ src/parser/nodes/tokens.ts
@@ -1,32 +1,28 @@
-import Definitions from "../../core/definitions";
-import Token from "../../tokenizer/tokens";
-import Node from "./nodes";
+import Definitions  from '../../core/definitions';
+import Token        from '../../tokenizer/tokens';
+import Node         from './nodes';
 
-
+/**
+ * Represents a token node. It represents the leaf nodes in the AST.
+ */
 class TokenNode extends Token implements Node {
   constructor (token: Token) {
     super();
-
-    this.pos = token.pos;
-    this.row = token.row;
-    this.col = token.col;
-    this.token = token.token;
-    this.value = token.value;
-    this.type = token.type;
-    this.subType = token.subType;
+    Object.assign(this, token);
   }
 
-
+  /**
+   * Returns the parsed value from the token.
+   * @param defs If the token is of type string, then check if it is a reference
+   *             and return the value from the definitions.
+   * @returns The parsed value.
+   */
   toValue(defs?: Definitions):any {
     if (this.type === 'string' && defs !== undefined) {
       const valueFound = defs.getV(this.value);
       return valueFound === undefined ? this.value : valueFound;
     }
     return this.value;
-  }
-
-  asToken(): Token {
-    return this;
   }
 }
 
