5e5fa0afea8fbfa806a4c53e1175c535e898dc72
Aamir Maniar <aamironline@gmail.com>
2023-12-13 15:52:21 +0530

    datetime parsing WIP

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -1,14 +1,18 @@
 
 export const datetimeExp = {
-  datetime: /^((\d{4})(?:\-((?:1[0-2]|0[1-9]))(?:\-([0-2][0-9]|3[0-1]))?)?(T([0-1][0-9]|2[0-3])(?:\:([0-5][0-9])(?:\:([0-5][0-9])(?:\.(\d{3}))?)?)?)?((Z)|((?:\+|-)(?:(?:[0-1][0-9]|2[0-3])(?:\:[0-5][0-9])?)))?)$/,
-  date: /^((\d{4})(?:\-((?:1[0-2]|0[1-9]))(?:\-([0-2][0-9]|3[0-1]))?)?)$/,
-  time: /^(([0-1][0-9]|2[0-3])(?:\:([0-5][0-9])(?:\:([0-5][0-9])(?:\.(\d{3}))?)?)?)$/
+  // https://regex101.com/r/v1YLhA/1
+  datetime: /^(?<dt>(?<year>\d{4})(?:\-(?<month>(?:1[0-2]|0[1-9]))(?:\-(?<date>[0-2][0-9]|3[0-1]))?)?(T(?<hour>[0-1][0-9]|2[0-3])(?:\:(?<minute>[0-5][0-9])(?:\:(?<sec>[0-5][0-9])(?:\.(?<milisecond>(?:\d{3})+))?)?)?)?(?<tz>(Z)|((?:\+|-)(?:(?:[0-1][0-9]|2[0-3])(?:\:[0-5][0-9])?)))?)/,
+
+  // https://regex101.com/r/fDYSLf/1
+  time: /T(?<hour>[0-1][0-9]|2[0-3])(?:\:(?<minute>[0-5][0-9])(?:\:(?<second>[0-5][0-9])(?:\.(?<milisecond>(?:\d{3})+))?)?)?/
 }
 
 export const datetimePlainExp = {
-  datetime: /^((\d{4})(?:((?:1[0-2]|0[1-9]))(?:([0-2][0-9]|3[0-1]))?)?(T([0-1][0-9]|2[0-3])(?:([0-5][0-9])(?:([0-5][0-9])(?:\.(\d{3}))?)?)?)?((Z)|((?:\+|-)(?:(?:[0-1][0-9]|2[0-3])(?:[0-5][0-9])?)))?)$/,
-  date: /^((\d{4})(?:((?:1[0-2]|0[1-9]))(?:([0-2][0-9]|3[0-1]))?)?)$/,
-  time: /^(([0-1][0-9]|2[0-3])(?:([0-5][0-9])(?:([0-5][0-9])(?:\.(\d{3}))?)?)?)$/
+  // https://regex101.com/r/0j7nlS/3
+  datetime: /^(?<year>\d{4})(?:(?<month>(?:1[0-2]|0[1-9]))(?:(?<date>[0-2][0-9]|3[0-1]))?)?(?:(?<hour>[0-1][0-9]|2[0-3])(?:(?<minute>[0-5][0-9])(?:(?<second>[0-5][0-9])(?:(?<milisecond>(?:\d{3})+))?)?)?)?(?<tz>(Z)|((?:\+|-)(?:(?:[0-1][0-9]|2[0-3])(?:[0-5][0-9])?)))?/,
+
+  // https://regex101.com/r/X5AA4A/2
+  time: /^T(?<hour>[0-1][0-9]|2[0-3])(?:(?<minute>[0-5][0-9])(?:(?<second>[0-5][0-9])(?:(?<milisecond>(?:\d{3})+))?)?)?/
 }
 
 /**
@@ -17,22 +21,53 @@ export const datetimePlainExp = {
  * is found.
  */
 export const parseDateTime = (value: string): Date | null => {
-  const exp = /[\-\:]/.test(value) ? datetimeExp.datetime : datetimePlainExp.datetime
-  const d = exp.exec(value)
-  if (d === null) {
-    return null
+  // https://regex101.com/r/P7DNUL/2
+  const match = /^(?<dt>(?<year>\d{4})(?:\-(?<month>(?:1[0-2]|0[1-9]))(?:\-(?<date>[0-2][0-9]|3[0-1]))?)?(T(?<hour>[0-1][0-9]|2[0-3])(?:\:(?<minute>[0-5][0-9])(?:\:(?<second>[0-5][0-9])(?:\.(?<milisecond>(\d{3})+))?)?)?)?(?<tz>(Z)|((?:\+|-)(?:(?:[0-1][0-9]|2[0-3])(?:\:[0-5][0-9])?)))?)|(?<tm>T(?<hour2>[0-1][0-9]|2[0-3])(?:\:(?<minute2>[0-5][0-9])(?:\:(?<second2>[0-5][0-9])(?:\.(?<milisecond2>(\d{3})+))?)?)?)$/gm.exec(value);
+
+  if (!match) {
+    console.error('Invalid date/time format:', value);
+    return null;
   }
 
-  // When time signature is not defined, makr the date as utc date
-  // otherwise read the utc signature from the value.
-  const utc = typeof d[5] === 'undefined' ? 'Z' : d[10] || ''
+  const { year, month, date, hour, minute, second, milisecond, tz, hour2, minute2, second2, milisecond2 } = match.groups || {};
 
-  const dateStr =
-    `${d[2]}-${d[3] || '01'}-${d[4] || '01'}` +
-    `T${d[6] || '00'}:${d[7] || '00'}:${d[8] || '00'}.${d[9] || '000'}${utc}`
+  if (year) {
+    const utc = tz === 'Z' ? 'Z' : tz || '';
+    const dateStr = `${year}-${month || '01'}-${date || '01'}T${hour || '00'}:${minute || '00'}:${second || '00'}.${milisecond || '000'}${utc}`;
 
-  return new Date(dateStr)
+    return new Date(dateStr);
+  }
+
+  if (hour2) {
+    const dateStr = `1900-01-01T${hour2 || '00'}:${minute2 || '00'}:${second2 || '00'}.${milisecond2 || '000'}`;
+
+    return new Date(dateStr);
+  }
+
+  console.error('Invalid date/time format - missing year:', value);
+  return null;
 }
+// export const parseDateTime = (value: string): Date | null => {
+//   const exp = /[\-\:]/.test(value) ? datetimeExp.datetime : datetimePlainExp.datetime;
+//   const match = exp.exec(value);
+
+//   if (!match) {
+//     console.error('Invalid datetime format:', value);
+//     return null;
+//   }
+
+//   const { year, month, date, hour, minute, second, milisecond, tz } = match.groups || {};
+
+//   if (!year) {
+//     console.error('Year is required in datetime format:', value);
+//     return null;
+//   }
+
+//   const utc = tz === 'Z' ? 'Z' : tz || '';
+//   const dateStr = `${year}-${month || '01'}-${date || '01'}T${hour || '00'}:${minute || '00'}:${second || '00'}.${milisecond || '000'}${utc}`;
+
+//   return new Date(dateStr);
+// }
 
 /**
  * Parses the value string and returns the datetime if the string represents
@@ -40,14 +75,23 @@ export const parseDateTime = (value: string): Date | null => {
  * is found.
  */
 export const parseDate = (value: string): Date | null => {
-  const exp = /\-/.test(value) ? datetimeExp.date : datetimePlainExp.date
-  const d = exp.exec(value)
-  if (d === null) {
-    return null
+  const exp = /\-/.test(value) ? datetimeExp.date : datetimePlainExp.date;
+  const match = exp.exec(value);
+
+  if (!match) {
+    console.error('Invalid date format:', value);
+    return null;
   }
 
-  const dateStr = `${d[2]}-${d[3] || '01'}-${d[4] || '01'}`
-  return new Date(dateStr)
+  const { year, month, date } = match.groups || {};
+
+  if (!year) {
+    console.error('Year is required in date format:', value);
+    return null;
+  }
+
+  const dateStr = `${year}-${month || '01'}-${date || '01'}`;
+  return new Date(dateStr);
 }
 
 /**
@@ -56,14 +100,18 @@ export const parseDate = (value: string): Date | null => {
  * is found.
  */
 export const parseTime = (value: string): Date | null => {
-  const exp = /\:/.test(value) ? datetimeExp.time : datetimePlainExp.time
-  const d = exp.exec(value)
-  if (d === null) {
-    return null
+  const exp = /\:/.test(value) ? datetimeExp.time : datetimePlainExp.time;
+  const match = exp.exec(value);
+
+  if (!match) {
+    console.error('Invalid time format:', value);
+    return null;
   }
 
-  const dateStr = `1900-01-01T${d[2]}:${d[3] || '00'}:${d[4] || '00'}.${d[5] || '000'}`
-  return new Date(dateStr)
+  const { hour, minute, second, milisecond } = match.groups || {};
+
+  const dateStr = `1900-01-01T${hour || '00'}:${minute || '00'}:${second || '00'}.${milisecond || '000'}`;
+  return new Date(dateStr);
 }
 
 export const dateToDatetimeString = (date: Date | null, noSep = false, zuluTime = false) => {
