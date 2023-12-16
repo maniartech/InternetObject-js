7ff233143417add1bfa504026945f5a22bd28a4d
Aamir Maniar <aamironline@gmail.com>
2023-12-16 14:12:35 +0530

    Datetime parsing impreovements

--- src/tokenizer/symbols.ts
+++ src/tokenizer/symbols.ts
@@ -9,9 +9,11 @@ enum Symbols {
   BRACKET_CLOSE   = ']',
   COLON           = ':',
   COMMA           = ',',
-  R               = 'r',
-  B               = 'b',
-  D               = 'd',
+  R               = 'r', // raw string
+  B               = 'b', // binary
+  DT              = 'dt', // datetime
+  D               = 'd', // date
+  T               = 't', // time
   HASH            = '#',
   DOUBLE_QUOTE    = '"',
   SINGLE_QUOTE    = "'",
