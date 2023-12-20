3a07f4a5399a2e851c3862967390556af7f2adeb
Aamir Maniar <aamironline@gmail.com>
2023-12-20 13:52:02 +0530

    TypeDef revamp

--- src/types/array.ts
+++ src/types/array.ts
@@ -1,13 +1,13 @@
 import Definitions            from '../core/definitions'
 import assertNever            from '../errors/asserts/asserts'
 import ErrorCodes             from '../errors/io-error-codes'
+import ValidationError        from '../errors/io-validation-error'
 import ArrayNode              from '../parser/nodes/array'
 import Node                   from '../parser/nodes/nodes'
 import TokenNode              from '../parser/nodes/tokens'
 import Schema                 from '../schema/schema'
 import TypeDef                from '../schema/typedef'
 import TypedefRegistry        from '../schema/typedef-registry'
-import TokenType              from '../tokenizer/token-types'
 import MemberDef              from './memberdef'
 import doCommonTypeCheck      from './common-type'
 
@@ -29,19 +29,22 @@ class ArrayDef implements TypeDef {
 
   get schema() { return schema }
 
-  public parse = (node: Node, memberDef: MemberDef, defs?: Definitions, collectionIndex?: number): any => {
-    const value = node instanceof ArrayNode || node instanceof TokenNode ? node : undefined
-    return _processNode(memberDef, value, node, defs, collectionIndex)
+  public parse = (valueNode: Node, memberDef: MemberDef, defs?: Definitions, collectionIndex?: number): any => {
+    return _processNode(valueNode, memberDef, defs, collectionIndex)
   }
 }
 
 function _processNode(
+  node: Node,
   memberDef: MemberDef,
-  value: any,
-  node?: Node,
   defs?: Definitions,
   collectionIndex?: number
 ) {
+  const valueNode = defs?.getV(node) || node
+  const { value, changed } = doCommonTypeCheck(memberDef, valueNode, node, defs)
+  if (changed) return value
+
+
   // Find the right typeDef
   let typeDef: TypeDef | undefined
   let arrayMemberDef: MemberDef = {
@@ -57,22 +60,13 @@ function _processNode(
     typeDef = TypedefRegistry.get('any')
   }
 
+
+  if (valueNode instanceof ArrayNode === false) {
+    throw new ValidationError(ErrorCodes.notAnArray, `Expecting an array value for '${memberDef.path}'`, node)
+  }
+
   const array: any = []
-
-  // Check if the value is a definition string starting with $
-  // If yes, then get the value from the definitions
-  if (node instanceof TokenNode && node.type === TokenType.STRING) {
-    value = defs?.getV(node.value)
-  } else if (!!value && value instanceof ArrayNode === false) {
-    throw new Error('invalid-value')
-  }
-
-  const validatedData = doCommonTypeCheck(memberDef, value, node)
-  if (validatedData !== node || validatedData === null || validatedData === undefined) {
-    return validatedData
-  }
-
-  value.children.forEach((item: any) => {
+  valueNode.children.forEach((item: any) => {
     const value = typeDef?.parse(item, arrayMemberDef, defs)
     array.push(value)
   })
