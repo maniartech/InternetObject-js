087662b82b4490ecd2f823a7224954239ed10451
Aamir Maniar <aamironline@gmail.com>
2023-12-24 18:54:40 +0530

    Refactor parseDefinitions function to handle null externalDefs

--- src/parser/parse-defs.ts
+++ src/parser/parse-defs.ts
@@ -4,15 +4,22 @@ import Definitions    from "../core/definitions"
 
 
 export default function parseDefinitions(
-    source: string, externalDefs: Definitions, options?: ParserOptions
+    source: string, externalDefs: Definitions | null, options?: ParserOptions
   ): Definitions | null {
 
+  source = source.trim()
   if (!source) { return null }
 
-  source = source.trimEnd()
   if (!source.endsWith("---")) {
     source += "\n---"
   }
+
+  // If the definition does not start with ~, then it must be
+  // a default schema. So, add "~ $schema: " in the beginning to make it a
+  // definition.
+  // if (!source.startsWith("~")) {
+  //   source = "~ $schema: " + source
+  // }
 
   const doc = parse(source, externalDefs, options)
   return doc.header.definitions
