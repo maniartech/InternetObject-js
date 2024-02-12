2b70e5032c89bc1dee7764cc60c9b888cef77667
Aamir Maniar <aamironline@gmail.com>
2024-02-12 12:13:09 +0530

    Add iosf namespace and helper functions

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,36 @@
 //         ██║ ██║     ██║
 //         ██║ ╚████████╔╝
 //         ╚═╝  ╚═══════╝
+
+import Definitions      from './core/definitions';
+import Document         from './core/document';
+import parse            from './parser/index';
+import parseDefinitions from './parser/parse-defs';
+
+namespace iosf {
+  export function doc(strings: TemplateStringsArray, ...args: any[]):Document {
+    const input = strings.reduce((acc, str, i) => {
+      return acc + str + (args[i] === undefined ? '' : args[i]);
+    }, '');
+
+    return parse(input, null);
+  }
+
+  export function object(strings: TemplateStringsArray, ...args: any[]):any {
+    const input = strings.reduce((acc, str, i) => {
+      return acc + str + (args[i] === undefined ? '' : args[i]);
+    }, '');
+
+    return parse(input, null).toObject();
+  }
+
+  export function defs(strings: TemplateStringsArray, ...args: any[]):Definitions | null {
+    const input = strings.reduce((acc, str, i) => {
+      return acc + str + (args[i] === undefined ? '' : args[i]);
+    }, '');
+
+    return parseDefinitions(input, null);
+  }
+}
+
+export default iosf;
