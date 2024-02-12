05f0a47a4006f2379bb323d28a0c94c694581644
Aamir Maniar <aamironline@gmail.com>
2024-02-12 12:48:09 +0530

    Add test for object parsing in IO Namespace

--- tests/namespace.test.ts
+++ tests/namespace.test.ts
@@ -17,4 +17,24 @@ describe('IO Namescpae', () => {
       address: { street: '123 Main St', city: 'Springfield', zip: 12345 }
     });
   });
+
+  test('should be able to parse the object', () => {
+    const defs = iosf.defs`
+      name, age, address: { street, city, zip }
+    `;
+
+    if (!defs) {
+      throw new Error('Defs is null');
+    }
+
+    const obj = iosf.parseWith(defs)`
+      John, 30, { ${123} Main St, Springfield, 12345 }
+    `;
+
+    expect(obj.toObject()).toEqual({
+      name: 'John',
+      age: 30,
+      address: { street: '123 Main St', city: 'Springfield', zip: 12345 }
+    });
+  });
 })
