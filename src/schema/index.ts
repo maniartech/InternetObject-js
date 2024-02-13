0e7d1b05e79274b15aed4f489013f2aa5a85121f
Aamir Maniar <aamironline@gmail.com>
2024-02-13 20:13:05 +0530

    Update import statement in schema/index.ts

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,5 +1,5 @@
 import ASTParser                      from '../parser/ast-parser';
-import ObjectNode                     from '../parser/nodes/objects';
+import Node                           from '../parser/nodes/nodes';
 import Tokenizer                      from '../tokenizer';
 import Schema                         from './schema';
 import compileObject                  from './compile-object';
@@ -7,5 +7,5 @@ import compileObject                  from './compile-object';
 export default function compileSchema(name:string, schema: string): Schema {
   const tokens = new Tokenizer(schema).tokenize();
   const ast = new ASTParser(tokens).parse()
-  return compileObject(name, ast.children[0].child as ObjectNode);
+  return compileObject(name, ast.children[0].child as Node);
 }
