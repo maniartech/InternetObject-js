11c38395ce2d8ac6bb1c66b7687104ec0641644f
Aamir Maniar <aamironline@gmail.com>
2024-02-13 20:12:47 +0530

    Refactor imports and export functions in index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,45 +7,63 @@
 //         ██║ ╚████████╔╝
 //         ╚═╝  ╚═══════╝
 
-import Definitions      from './core/definitions';
-import Document         from './core/document';
-import parse            from './parser/index';
+import Definitions from './core/definitions';
+import Document from './core/document';
+import parse from './parser/index';
 import parseDefinitions from './parser/parse-defs';
+import InternetObject from './core/internet-object';
+import Schema from './schema/schema';
+import Collection from './core/collection';
+import InternetObjectError from './errors/io-error';
+import InternetObjectSyntaxError from '../dist/errors/syntax-error';
+import InternetObjectValidationError from './errors/io-validation-error';
+import SectionCollection from './core/section-collection';
+import Section from './core/section';
 
-namespace iosf {
-  export function doc(strings: TemplateStringsArray, ...args: any[]):Document {
+export {
+  Document,
+  Definitions,
+  SectionCollection,
+  Section,
+  Collection,
+  InternetObject,
+  Schema,
+  InternetObjectError,
+  InternetObjectSyntaxError,
+  InternetObjectValidationError
+};
+
+export function ioDocument(strings: TemplateStringsArray, ...args: any[]): Document {
+  const input = strings.reduce((acc, str, i) => {
+    return acc + str + (args[i] === undefined ? '' : args[i]);
+  }, '');
+
+  return parse(input, null);
+}
+
+ioDocument.with = (defs: Definitions): (strings: TemplateStringsArray, ...args: any[]) => Document => {
+  return (strings: TemplateStringsArray, ...args: any[]) => {
     const input = strings.reduce((acc, str, i) => {
       return acc + str + (args[i] === undefined ? '' : args[i]);
     }, '');
 
-    return parse(input, null);
-  }
-
-  export function object(strings: TemplateStringsArray, ...args: any[]):any {
-    const input = strings.reduce((acc, str, i) => {
-      return acc + str + (args[i] === undefined ? '' : args[i]);
-    }, '');
-
-    return parse(input, null).toObject();
-  }
-
-  export function defs(strings: TemplateStringsArray, ...args: any[]):Definitions | null {
-    const input = strings.reduce((acc, str, i) => {
-      return acc + str + (args[i] === undefined ? '' : args[i]);
-    }, '');
-
-    return parseDefinitions(input, null);
-  }
-
-  export function parseWith(defs: Definitions): (strings: TemplateStringsArray, ...args: any[]) => Document {
-    return (strings: TemplateStringsArray, ...args: any[]) => {
-      const input = strings.reduce((acc, str, i) => {
-        return acc + str + (args[i] === undefined ? '' : args[i]);
-      }, '');
-
-      return parse(input, defs);
-    }
+    return parse(input, defs);
   }
 }
 
-export default iosf;
+export function ioObject(strings: TemplateStringsArray, ...args: any[]): any {
+  const input = strings.reduce((acc, str, i) => {
+    return acc + str + (args[i] === undefined ? '' : args[i]);
+  }, '');
+
+  return parse(input, null).toObject();
+}
+
+export function ioDefinitions(strings: TemplateStringsArray, ...args: any[]): Definitions | null {
+  const input = strings.reduce((acc, str, i) => {
+    return acc + str + (args[i] === undefined ? '' : args[i]);
+  }, '');
+
+  return parseDefinitions(input, null);
+}
+
