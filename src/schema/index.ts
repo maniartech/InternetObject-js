d48a065d589765154fcb6ad5a99ba49b21366009
Aamir Maniar <aamironline@gmail.com>
2024-03-22 15:18:33 +0530

    Add assertNever function to handle invalid schema type

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,3 +1,4 @@
+import assertNever                    from '../errors/asserts/asserts';
 import ASTParser                      from '../parser/ast-parser';
 import Node                           from '../parser/nodes/nodes';
 import Tokenizer                      from '../tokenizer';
@@ -7,5 +8,10 @@ import compileObject                  from './compile-object';
 export default function compileSchema(name:string, schema: string): Schema {
   const tokens = new Tokenizer(schema).tokenize();
   const ast = new ASTParser(tokens).parse()
-  return compileObject(name, ast.children[0].child as Node);
+  const node = compileObject(name, ast.children[0].child as Node);
+  if (node instanceof Schema) {
+    return node;
+  }
+
+  assertNever("Invalid schema type");
 }
