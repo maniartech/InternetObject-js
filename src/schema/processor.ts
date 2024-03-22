ca3b9395e36b1c4af74133867804f1729f9ed713
Aamir Maniar <aamironline@gmail.com>
2024-03-22 15:19:33 +0530

    Refactor processSchema function to handle schema references

--- src/schema/processor.ts
+++ src/schema/processor.ts
@@ -1,15 +1,42 @@
 import Collection       from '../core/collection';
 import Definitions      from '../core/definitions';
 import InternetObject   from '../core/internet-object';
+import InternetObjectError from '../errors/io-error';
+import ErrorCodes from '../errors/io-error-codes';
 import CollectionNode   from '../parser/nodes/collections';
 import ObjectNode       from '../parser/nodes/objects';
+import TokenNode from '../parser/nodes/tokens';
+import TokenType from '../tokenizer/token-types';
+import compileObject from './compile-object';
 import processObject    from './object-processor';
 import Schema           from './schema';
 
-export default function processSchema(data:ObjectNode | CollectionNode | null, schema: Schema, defs?: Definitions) {
+export default function processSchema(data:ObjectNode | CollectionNode | null, schema: Schema | TokenNode, defs?: Definitions) {
   if (!data) {
     return null;
   }
+
+  // If schema is TokenNode of string, then it is a reference to a schema
+  // Fetch the schema from definitions
+  // if (schema instanceof TokenNode && schema.type === TokenType.STRING) {
+  //   const schemaRef = schema.value;
+  //   if (!schemaRef.startsWith("$")) {
+  //     throw new InternetObjectError(ErrorCodes.invalidSchema, `Invalid schema reference '${schemaRef}'`);
+  //   }
+
+  //   const foundSchema = defs?.getV(schemaRef) as Schema;
+  //   if (!foundSchema) {
+  //     throw new InternetObjectError(ErrorCodes.schemaNotFound, `Schema '${schemaRef}' not found`, schemaRef);
+  //   }
+
+  //   if (foundSchema instanceof ObjectNode) {
+  //     schema = compileObject(schemaRef, foundSchema, defs);
+  //     defs!.set(schemaRef, schema);
+  //   } else {
+  //     console.log(">>>", defs)
+  //     throw new InternetObjectError(ErrorCodes.invalidSchema, `Invalid schema reference '${schemaRef}'`);
+  //   }
+  // }
 
   if (data instanceof ObjectNode) {
     return processObject(data, schema, defs);
@@ -20,7 +47,7 @@ export default function processSchema(data:ObjectNode | CollectionNode | null, s
   }
 }
 
-function processCollection(data: CollectionNode, schema: Schema, defs?: Definitions) {
+function processCollection(data: CollectionNode, schema: Schema | TokenNode, defs?: Definitions) {
   const coll = new Collection<InternetObject>();
   for (let i=0; i<data.children.length; i++) {
     const member = data.children[i] as ObjectNode;
