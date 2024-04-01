1afbfd0c44cbf0d8a1d0570b1a2357ffd4cb8de4
Aamir Maniar <aamironline@gmail.com>
2024-04-02 02:18:04 +0530

    Refactor object processing logic to handle positional and keyed members

--- src/schema/object-processor.ts
+++ src/schema/object-processor.ts
@@ -5,10 +5,10 @@ import SyntaxError        from '../errors/io-syntax-error';
 import MemberNode         from '../parser/nodes/members';
 import ObjectNode         from '../parser/nodes/objects';
 import MemberDef          from '../types/memberdef';
-import TypedefRegistry    from './typedef-registry';
-import Schema             from './schema';
 import TokenNode          from '../parser/nodes/tokens';
 import assertNever        from '../errors/asserts/asserts';
+import TypedefRegistry    from './typedef-registry';
+import Schema             from './schema';
 
 export default function processObject(data: ObjectNode, schema: Schema | TokenNode, defs?: Definitions, collectionIndex?: number) {
   if (schema instanceof TokenNode) {
@@ -23,66 +23,79 @@ export default function processObject(data: ObjectNode, schema: Schema | TokenNo
 
   return _processObject(data, schema as Schema, defs, collectionIndex);
 }
-
 function _processObject(data: ObjectNode, schema: Schema, defs?: Definitions, collectionIndex?: number) {
   const o: InternetObject = new InternetObject();
   let positional = true;
-
   const processedNames = new Set<string>();
-  for (let i=0; i<schema.names.length; i++) {
+
+  // Process positional schema members
+  let i=0;
+  for (; i<schema.names.length; i++) {
     let member = data.children[i] as MemberNode;
-    let name = schema.names[i];
+
+    if (member) {
+      if (member.key) {
+        positional = false;
+        break;
+      }
+
+      let name = schema.names[i];
+      let memberDef = schema.defs[name];
+
+      processedNames.add(name);
+
+      const val = processMember(member, memberDef, defs);
+      if (val !== undefined) o[name] = val;
+    }
+  }
+
+  // Process remaining positional members
+  if (positional) {
+    for (; i<data.children.length; i++) {
+      const member = data.children[i] as MemberNode;
+      if (member.key) {
+        positional = false;
+        break;
+      }
+
+      const val = member.value.toValue(defs)
+      o.push(val);
+    }
+  }
+
+  // Process remaining keyed members
+  for (; i<data.children.length; i++) {
+    let member = data.children[i] as MemberNode;
+
+    if (!member.key) {
+      throw new SyntaxError(ErrorCodes.unexpectedPositionalMember, "Positional members must not be allowed after the keyed member is found.", member);
+    }
+
+    let name = member.key.value;
     let memberDef = schema.defs[name];
 
-    if (member) {
-      if (member.key) {
-        if (positional) positional = false;
-        name = member.key.value;
-        memberDef = schema.defs[name];
-        processedNames.add(name);
-      } else {
-        // Once the keyed member is found after the positional flag is set to
-        // false, the positional members must not be allowed.
-        if (positional === false) {
-          throw new SyntaxError(ErrorCodes.unexpectedPositionalMember, "Positional members must not be allowed after the keyed member is found.", member);
-        }
-      }
-    } else {
-      // TODO: If the member is not found, then we must provide way to
-      // show the current object in the error state.
+    if (processedNames.has(name)) {
+      throw new SyntaxError(ErrorCodes.duplicateMember, `Member ${name} is already defined.`, member);
     }
+
+    // When the member is not found check if the schema is open to allow
+    // additional properties. If not throw an error.
+    if (!memberDef && !schema.open) {
+      throw new SyntaxError(
+        ErrorCodes.unknownMember, `Member ${name} is not defined in the schema.`, member);
+    }
+
+    processedNames.add(name);
 
     const val = processMember(member, memberDef, defs);
-    if (val !== undefined) o[name] = val;
+    o[name] = val;
   }
 
-  // If schema supports additional properties, then add them to the object
-  // if any. Otherwise  ignore the additional properties if any,
-  // and return the object as it is.
-  if (!schema.open) return o
-
-  for (let i=schema.names.length; i < data.children.length; i += 1) {
-    const member = data.children[i] as MemberNode;
-    if (member) {
-      if (member.key) {
-        const name = member.key.value;
-        o[name] = member.value.toValue(defs);
-      } else {
-        o.push(member.value.toValue(defs));
-      }
-    }
-  }
-
-  return o
+  return o;
 }
 
 function processMember(member: MemberNode, memberDef: MemberDef, defs?: Definitions): any {
   const typeDef = TypedefRegistry.get(memberDef.type);
-
-  // console.log(member, memberDef)
-  if (memberDef.path === 'of') {
-    debugger
-  }
 
   if (!typeDef) {
     throw new Error(`Type ${memberDef.type} is not registered.`);
