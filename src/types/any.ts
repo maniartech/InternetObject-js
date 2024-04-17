5932bf3dc0451b0610b58cc49d727c14b4e32d8c
Aamir Maniar <aamironline@gmail.com>
2024-04-17 23:07:17 +0530

    Add support anyOf constraints for "any"

--- src/types/any.ts
+++ src/types/any.ts
@@ -1,15 +1,23 @@
-import Definitions              from '../core/definitions'
-import Node                     from '../parser/nodes/nodes'
-import Schema                   from '../schema/schema'
-import TypeDef                  from '../schema/typedef'
-import doCommonTypeCheck        from './common-type'
-import MemberDef                from './memberdef'
+import Definitions            from '../core/definitions';
+import ErrorCodes             from '../errors/io-error-codes';
+import ValidationError        from '../errors/io-validation-error';
+import Node                   from '../parser/nodes/nodes';
+import { getMemberDef       } from '../schema/compile-object';
+import Schema                 from '../schema/schema';
+import TypeDef                from '../schema/typedef';
+import TypedefRegistry        from '../schema/typedef-registry';
+import doCommonTypeCheck      from './common-type';
+import MemberDef              from './memberdef';
+
+const of = { type: "any", __forMemberDef: true }
 
 const schema = new Schema(
   "any",
   { type:     { type: "string", optional: false, null: false, choices: ["any"] } },
-  { default:  { type: "any",    optional: true,  null: false  } },
+  { default:  { type: "any",    optional: true,  null: false } },
   { choices:  { type: "array",  optional: true,  null: false } },
+  { anyOf:    { type: "array",  optional: true,  null: false, of } },
+  { isSchema: { type: "bool",   optional: true,  null: false, default: false } },
   { optional: { type: "bool",   optional: true,  null: false, default: false } },
   { null:     { type: "bool",   optional: true,  null: false, default: false } },
 )
@@ -24,7 +32,41 @@ export default class AnyDef implements TypeDef {
     const { value, changed } = doCommonTypeCheck(memberDef, valueNode, node, defs)
     if (changed) return value
 
-    return value
+    const anyOf = memberDef.anyOf;
+    if (!anyOf) {
+      if (memberDef.__forMemberDef) { // Convert to memberDef
+        const md = getMemberDef(node, "", "", defs)
+        return md
+      }
+
+      return value
+    }
+
+    const errors = []
+
+    for (let i=0; i<anyOf.length; i++) {
+      const def = anyOf[i]
+      def.path = memberDef.path
+
+      const typeDef = TypedefRegistry.get(def.type)
+      if (!typeDef) {
+        throw new Error(`Invalid type definition '${def.type}'`)
+      }
+
+      try {
+        return typeDef.parse(node, def, defs, collectionIndex)
+      } catch (e) {
+        errors.push(e)
+        continue
+      }
+    }
+
+    // None of the types matched
+    if (errors.length === anyOf.length) {
+      throw new ValidationError(ErrorCodes.invalidValue, `None of the constraints defined for '${memberDef.path}' matched.`, node)
+    }
+
+    return valueNode;
   }
 
   public static get types() { return ['any'] }
