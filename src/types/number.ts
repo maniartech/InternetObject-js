c8246029b62308f5877a41054258ad956cea6623
Aamir Maniar <aamironline@gmail.com>
2024-04-19 17:00:18 +0530

    Refactor NumberDef

--- src/types/number.ts
+++ src/types/number.ts
@@ -1,11 +1,9 @@
 import Definitions          from '../core/definitions'
 import ValidationError      from '../errors/io-validation-error'
-import ErrorArgs            from '../errors/error-args'
 import ErrorCodes           from '../errors/io-error-codes'
 import Node                 from '../parser/nodes/nodes'
 import TokenNode            from '../parser/nodes/tokens'
 import Schema               from '../schema/schema'
-import TokenType            from '../tokenizer/token-types'
 import MemberDef            from './memberdef'
 import TypeDef              from '../schema/typedef'
 import doCommonTypeCheck    from './common-type'
@@ -19,10 +17,6 @@ const NUMBER_TYPES = [
   'float32', 'float64'                    // Floating point number types
 ]
 
-const isUnsupportedNumberType = [
-  'int64', 'uint64', 'float32'   // Unsupported number types by JavaScript
-]
-
 const schema = new Schema(
   "number",
   { type:     { type: "string", optional: false, null: false, choices: NUMBER_TYPES } },
@@ -33,8 +27,6 @@ const schema = new Schema(
   { optional: { type: "bool",   optional: true,  null: false, default: false } },
   { null:     { type: "bool",   optional: true,  null: false, default: false } },
 )
-
-// age?: { number, true, 10, min:10, max:20}
 
 /**
  * Represents the various number related data types in Internet Object.
@@ -56,66 +48,100 @@ class NumberDef implements TypeDef {
   get schema() { return schema }
 
   parse(node: Node, memberDef: MemberDef, defs?: Definitions): number {
-    return _validate(this._validator, node, memberDef, defs)
+    return this.#validate(this._validator, node, memberDef, defs)
+  }
+
+  // Performs following validations.
+  //  * - Value is number, int, int32, int16, byte
+  //  * - Value is optional
+  //  * - Value is nullable
+  //  * - Value >= schema.min
+  //  * - Value <= schema.max
+  //  * - Value is in choices
+  #validate(
+    validator: any,
+    node: Node, memberDef: MemberDef, defs?: Definitions
+  ) {
+    const valueNode = defs?.getV(node) || node
+    const { value, changed } = doCommonTypeCheck(memberDef, valueNode, node, defs)
+    if (changed) return value
+
+    const valueType = typeof value === "bigint" ? "bigint" : "number"
+    const memberdefType = memberDef.type === "bigint" ? "bigint" : "number"
+
+    if (valueNode instanceof TokenNode === false || memberdefType !== valueType) {
+      throw new ValidationError(
+        `not-a-${memberDef.type}`,
+        `Invalid value encountered for '${memberDef.path}'`,
+        node
+      )
+    }
+
+    validator(memberDef, value, node)
+
+    if (memberDef.min !== undefined) {
+      const min = memberDef.min
+      if (memberDef.min !== undefined && value < min) {
+        throw new ValidationError(
+          ErrorCodes.invalidMinValue,
+          `The '${memberDef.path}' must be greater than or equal to ${min}, Currently it is ${value}.`,
+          node
+        )
+      }
+    }
+
+    if (memberDef.max !== undefined && value > memberDef.max) {
+      throw new ValidationError(
+        ErrorCodes.invalidMaxValue,
+        `The '${memberDef.path}' must be less than or equal to ${memberDef.max}, Currently it is ${value}.`,
+        node
+      )
+    }
+
+    return value
   }
 }
 
-// Performs following validations.
-//  * - Value is number, int, int32, int16, byte
-//  * - Value is optional
-//  * - Value is nullable
-//  * - Value >= schema.min
-//  * - Value <= schema.max
-//  * - Value is in choices
-function _validate(
-  validator: any,
-  node: Node, memberDef: MemberDef, defs?: Definitions
-) {
-  const valueNode = defs?.getV(node) || node
-  const { value, changed } = doCommonTypeCheck(memberDef, valueNode, node, defs)
-  if (changed) return value
+function _intValidator(min: number | null, max: number | null, memberDef: MemberDef, value: any, node?: Node) {
+  if (min === null || max === null) return
 
-  if (valueNode instanceof TokenNode === false && valueNode.type !== TokenType.NUMBER) {
+  if (value % 1 !== 0) {
     throw new ValidationError(
-      ErrorCodes.notANumber,
-      `Invalid number encountered for ${memberDef.path}`,
+      ErrorCodes.notAnInteger,
+      `Expecting an integer value for '${memberDef.path}', Currently it is ${value}.`,
       node
     )
   }
 
-  validator(memberDef, value, node)
-
-  if (memberDef.min !== undefined) {
-    const min = memberDef.min
-    if (memberDef.min !== undefined && value < min) {
-      throw new ValidationError(..._invlalidMin(memberDef, value, node))
-    }
+  if (min !== null && value < min) {
+    throw new ValidationError(
+      ErrorCodes.invalidMinValue,
+      `The '${memberDef.path}' must be greater than or equal to ${min}, Currently it is ${value}.`,
+      node
+    )
   }
 
-  if (memberDef.max !== undefined && value > memberDef.max) {
-    throw new ValidationError(..._invlalidMax(memberDef, value, node))
-  }
-
-  return value
-}
-
-function _intValidator(min: number, max: number, memberDef: MemberDef, value: any, node?: Node) {
-  // Validate for integer
-  if (value % 1 !== 0) {
-    throw new ValidationError(..._notAnInt(memberDef, value, node))
-  }
-
-  if ((min !== -1 && value < min) || (max !== -1 && value > max)) {
-    throw new ValidationError(..._outOfRange(memberDef, value, node))
+  if (max !== null && value > max) {
+    throw new ValidationError(
+      ErrorCodes.invalidMaxValue,
+      `The '${memberDef.path}' must be less than or equal to ${max}, Currently it is ${value}.`,
+      node
+    )
   }
 }
 
 function _getValidator(type: string) {
   switch (type) {
+    case 'float':
+    case 'float64':
     case 'number': {
       return (memberDef: MemberDef, value: any, node?: Node) => {
         if (typeof Number(value) !== 'number') {
-          throw new ValidationError(..._notANumber(memberDef, value, node))
+          throw new ValidationError(
+            ErrorCodes.notANumber,
+            `Expecting a number value for '${memberDef.path}', Currently it is ${value}.`,
+            node
+          )
         }
       }
     }
@@ -148,9 +174,14 @@ function _getValidator(type: string) {
       return _intValidator.bind(null, 0, range - 1)
     }
 
+    case 'uint': {
+      return _intValidator.bind(null, 0, null)
+    }
+
+    case 'bigint':
     case 'int': {
       // Any non fraction number!
-      return _intValidator.bind(null, -1, -1)
+      return _intValidator.bind(null, null, null)
     }
 
     // Unsupported number types because JavaScript doesn't support them
@@ -167,46 +198,6 @@ function _getValidator(type: string) {
       console.assert(false, 'Invalid number type!')
     }
   }
-}
-
-function _outOfRange(memberDef: MemberDef, value: any, node?: Node): ErrorArgs {
-  return [
-    ErrorCodes.outOfRange,
-    `The value (${value}) set for "${memberDef.path}" is out of range.`,
-    node
-  ]
-}
-
-function _notAnInt(memberDef: MemberDef, value: any, node?: Node): ErrorArgs {
-  return [
-    ErrorCodes.notAnInteger,
-    `Expecting an integer value for "${memberDef.path}", Currently it is ${value}.`,
-    node
-  ]
-}
-
-function _notANumber(memberDef: MemberDef, value: any, node?: Node): ErrorArgs {
-  return [
-    ErrorCodes.notANumber,
-    `Expecting a number value for "${memberDef.path}", Currently it is ${value}.`,
-    node
-  ]
-}
-
-function _invlalidMin(memberDef: MemberDef, value: any, node?: Node): ErrorArgs {
-  return [
-    ErrorCodes.invalidMinValue,
-    `The "${memberDef.path}" must be greater than or equal to ${memberDef.min}, Currently it is ${value}.`,
-    node
-  ]
-}
-
-function _invlalidMax(memberDef: MemberDef, value: any, node?: Node): ErrorArgs {
-  return [
-    ErrorCodes.invalidMaxValue,
-    `The "${memberDef.path}" must be less than or equal to ${memberDef.max}, Currently it is ${value}.`,
-    node
-  ]
 }
 
 export default NumberDef
