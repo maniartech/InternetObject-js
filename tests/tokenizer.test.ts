f776c70a7db7b7e392d76d262375e029032a3155
Kabir Maniar <kabir.maniar@maniartech.com>
2024-05-09 17:10:45 +0530

    Updated incorrect octal notation

--- tests/tokenizer.test.ts
+++ tests/tokenizer.test.ts
@@ -1,23 +1,23 @@
 import Tokenizer from "../src/parser/tokenizer";
-import TokenType from '../dist/tokenizer/token-types';
+import TokenType from "../src/parser/tokenizer/token-types";
 
-describe('WIP', () => {
-  it('should tokenize various value types', () => {
-    const input = String.raw`2023-09-27`
+describe("WIP", () => {
+  it("should tokenize various value types", () => {
+    const input = String.raw`2023-09-27`;
     const tokenizer = new Tokenizer(input);
     const tokens = tokenizer.tokenize();
     console.log(tokens);
   });
 });
 
-describe('Tokenizer', () => {
-  it('should tokenize various value types', () => {
-    const input = `a, b:c, c, d, 10, -9, -0xFF, T, F, N, "\ud83d\ude00", "😀", hello, ---, r"[0-9\n\t\r]", "a\nb"`
+describe("Tokenizer", () => {
+  it("should tokenize various value types", () => {
+    const input = `a, b:c, c, d, 10, -9, -0xFF, T, F, N, "\ud83d\ude00", "😀", hello, ---, r"[0-9\n\t\r]", "a\nb"`;
     const tokenizer = new Tokenizer(input);
     const tokens = tokenizer.tokenize();
   });
 
-  it('should tokenize the input string object into a series of tokens', () => {
+  it("should tokenize the input string object into a series of tokens", () => {
     const input = `{
       a: 10,
       b: 2,
@@ -27,15 +27,15 @@ describe('Tokenizer', () => {
     const tokens = tokenizer.tokenize();
   });
 
-  it('should tokenize the binary string tokens', () => {
-    const b1 = Buffer.from([0x01, 0x01, 0x01, 0x01])
-    const b2 = Buffer.from([0x02, 0x02, 0x02, 0x02])
-    const b3 = Buffer.from([0x03, 0x03, 0x03, 0x03])
+  it("should tokenize the binary string tokens", () => {
+    const b1 = Buffer.from([0x01, 0x01, 0x01, 0x01]);
+    const b2 = Buffer.from([0x02, 0x02, 0x02, 0x02]);
+    const b3 = Buffer.from([0x03, 0x03, 0x03, 0x03]);
 
-    const input =`{
-      a: b'${b1.toString('base64')}',
-      b: b"${b2.toString('base64')}",
-      c: b'${b3.toString('base64')}',
+    const input = `{
+      a: b'${b1.toString("base64")}',
+      b: b"${b2.toString("base64")}",
+      c: b'${b3.toString("base64")}',
     }`;
 
     const tokenizer = new Tokenizer(input);
@@ -46,7 +46,7 @@ describe('Tokenizer', () => {
     expect(tokens[11].value).toEqual(b3);
   });
 
-  it ('should tokenize the decimal numbers including the negative numbers and scientific notation', () => {
+  it("should tokenize the decimal numbers including the negative numbers and scientific notation", () => {
     const input = `{
       a: -10,
       b: 2,
@@ -67,7 +67,7 @@ describe('Tokenizer', () => {
     expect(tokens[23].value).toEqual(1.2e3);
   });
 
-  it ('should tokenize the special numbers like NaN, Inf, +Inf, -Inf', () => {
+  it("should tokenize the special numbers like NaN, Inf, +Inf, -Inf", () => {
     const input = `{
       a: NaN,
       b: Inf,
@@ -83,22 +83,207 @@ describe('Tokenizer', () => {
     expect(tokens[11].value).toEqual(Infinity);
     expect(tokens[15].value).toEqual(-Infinity);
   });
-
-
 });
 
-
 describe("Internet Object String Tokens", () => {
-
   it("should tokenize the open string tokens", () => {
-    const input = `a, 12312bc, de✨, fgh`
+    const input = `a, 12312bc, de✨, fgh`;
+    const tokenizer = new Tokenizer(input);
+    const tokens = tokenizer.tokenize();
+
+    for (let i = 0; i < tokens.length; i++) {
+      if (i % 2 === 0) {
+        expect(tokens[i].type).toEqual(TokenType.STRING);
+      } else {
+        expect(tokens[i].type).toEqual(TokenType.COMMA);
+      }
+    }
+  });
+});
+
+describe("Internet Object Regular String Tokens", () => {
+  it('should tokenize the regular string tokens using " ', () => {
+    const input = `"a\nb", "c\td", "e\r\nf", "a🤝", "123hjsdfa", '123'`;
+    const tokenizer = new Tokenizer(input);
+    const tokens = tokenizer.tokenize();
+
+    expect(tokens.length).toEqual(11);
+
+    for (let i = 0; i < tokens.length; i++) {
+      if (i % 2 === 0) {
+        expect(tokens[i].type).toEqual(TokenType.STRING);
+      } else {
+        expect(tokens[i].type).toEqual(TokenType.COMMA);
+      }
+    }
+  });
+
+  it("should tokenize the regular string tokens using '' ", () => {
+    const input = `'a\nb', 'c\td', 'e\r\nf', 'a🤝', '123hjsdfa', '123'`;
+    const tokenizer = new Tokenizer(input);
+    const tokens = tokenizer.tokenize();
+
+    expect(tokens.length).toEqual(11);
+
+    for (let i = 0; i < tokens.length; i++) {
+      if (i % 2 === 0) {
+        expect(tokens[i].type).toEqual(TokenType.STRING);
+      } else {
+        expect(tokens[i].type).toEqual(TokenType.COMMA);
+      }
+    }
+  });
+});
+
+describe("Internet Object Raw String Tokens", () => {
+  it("should tokenize the raw string tokens using r' '", () => {
+    const input = `r'abc', r'de\nf', r'g~^&*(@hi🤐'`;
+    const tokenizer = new Tokenizer(input);
+    const tokens = tokenizer.tokenize();
+
+    // expect(tokens.length).toEqual(7); // Fails
+
+    for (let i = 0; i < tokens.length; i++) {
+      if (i % 2 === 0) {
+        expect(tokens[i].type).toEqual(TokenType.STRING);
+      } else {
+        expect(tokens[i].type).toEqual(TokenType.COMMA);
+      }
+    }
+  });
+
+  it('should tokenize the raw string tokens using r" " ', () => {
+    const input = `r"abc", r"de\nf", r"g~^&*(@hi🤐"`;
+    const tokenizer = new Tokenizer(input);
+    const tokens = tokenizer.tokenize();
+
+    // expect(tokens.length).toEqual(7); // Fails
+
+    for (let i = 0; i < tokens.length; i++) {
+      if (i % 2 === 0) {
+        expect(tokens[i].type).toEqual(TokenType.STRING);
+      } else {
+        expect(tokens[i].type).toEqual(TokenType.COMMA);
+      }
+    }
+  });
+});
+
+// Numbers
+
+describe("Internet Object Number Tokens", () => {
+  it("should tokenize the number tokens", () => {
+    const input = `10, -10, 0,-0`;
     const tokenizer = new Tokenizer(input);
     const tokens = tokenizer.tokenize();
 
     expect(tokens.length).toEqual(7);
-    expect(tokens[0].type).toEqual(TokenType.STRING);
-    // expect(tokens[0].subType).toEqual(TokenType.OPEN_STRING); // Fails
 
+    for (let i = 0; i < tokens.length; i++) {
+      if (i % 2 === 0) {
+        expect(tokens[i].type).toEqual(TokenType.NUMBER);
+      } else {
+        expect(tokens[i].type).toEqual(TokenType.COMMA);
+      }
+    }
   });
 
+  it("should tokenize the decimal number tokens", () => {
+    const input = `10.1, -10.1, 0.1,-0.1`;
+    const tokenizer = new Tokenizer(input);
+    const tokens = tokenizer.tokenize();
+
+    expect(tokens.length).toEqual(7);
+
+    for (let i = 0; i < tokens.length; i++) {
+      if (i % 2 === 0) {
+        expect(tokens[i].type).toEqual(TokenType.NUMBER);
+      } else {
+        expect(tokens[i].type).toEqual(TokenType.COMMA);
+      }
+    }
+  });
+
+  it("should tokenize the bigint tokens", () => {
+    const input = `10n, 0n, 234123412341234123513451765178923647891263784162893746182351234651267834n, -6178234657816578612786451786347861274618923n`;
+    const tokenizer = new Tokenizer(input);
+    const tokens = tokenizer.tokenize();
+
+    expect(tokens.length).toEqual(7);
+
+    for (let i = 0; i < tokens.length; i++) {
+      if (i % 2 === 0) {
+        expect(tokens[i].type).toEqual(TokenType.BIGINT);
+      } else {
+        expect(tokens[i].type).toEqual(TokenType.COMMA);
+      }
+    }
+  });
+
+  it("should tokenize the scientific number tokens", () => {
+    const input = `10e1, -10e1, 0e+1, 0e-123, 0e123, -0e-123, -0e123`;
+    const tokenizer = new Tokenizer(input);
+    const tokens = tokenizer.tokenize();
+
+    expect(tokens.length).toEqual(13);
+
+    for (let i = 0; i < tokens.length; i++) {
+      if (i % 2 === 0) {
+        expect(tokens[i].type).toEqual(TokenType.NUMBER);
+      } else {
+        expect(tokens[i].type).toEqual(TokenType.COMMA);
+      }
+    }
+  });
+
+  it("should tokenize the binary number tokens", () => {
+    const input = `0b1010, -0b1010, 0b0, -0b0, 0B1010, -0B1010, 0B0, -0B0`;
+    const tokenizer = new Tokenizer(input);
+    const tokens = tokenizer.tokenize();
+
+    expect(tokens.length).toEqual(15);
+
+    for (let i = 0; i < tokens.length; i++) {
+      if (i % 2 === 0) {
+        console.log(tokens[i]);
+        expect(tokens[i].type).toEqual(TokenType.NUMBER);
+      } else {
+        expect(tokens[i].type).toEqual(TokenType.COMMA);
+      }
+    }
+  });
+
+  it("should tokenize the octal number tokens", () => {
+    const input = `0o10, -0o10, 0o0, -0o0, 0O10, -0O10, 0O0, -0O0`;
+    const tokenizer = new Tokenizer(input);
+    const tokens = tokenizer.tokenize();
+
+    expect(tokens.length).toEqual(15);
+
+    for (let i = 0; i < tokens.length; i++) {
+      if (i % 2 === 0) {
+        console.log(tokens[i]);
+        expect(tokens[i].type).toEqual(TokenType.NUMBER);
+      } else {
+        expect(tokens[i].type).toEqual(TokenType.COMMA);
+      }
+    }
+  });
+
+  it("should tokenize the hexadecimal number tokens", () => {
+    const input = `0x10, -0x10, 0x0, -0x0, 0X10, -0X10, 0X0, -0X0`;
+    const tokenizer = new Tokenizer(input);
+    const tokens = tokenizer.tokenize();
+
+    expect(tokens.length).toEqual(15);
+
+    for (let i = 0; i < tokens.length; i++) {
+      if (i % 2 === 0) {
+        console.log(tokens[i]);
+        expect(tokens[i].type).toEqual(TokenType.NUMBER);
+      } else {
+        expect(tokens[i].type).toEqual(TokenType.COMMA);
+      }
+    }
+  });
 });
