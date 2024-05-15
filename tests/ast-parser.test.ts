6352cacd23a31647814b8057edfabf30f92ca92b
Kabir Maniar <kabir.maniar@maniartech.com>
2024-05-15 15:04:39 +0530

    Added test cases for Variables in the header

--- tests/ast-parser.test.ts
+++ tests/ast-parser.test.ts
@@ -61,7 +61,7 @@ describe("AST Parser", () => {
     ).toEqual(true);
   });
 
-  it("should parse multiple documents", () => {
+  it("should parse collection of sections", () => {
     const input = `
     ~ a,b,c
     ~ 1,2,3
@@ -158,12 +158,44 @@ describe("AST Parser", () => {
     expect(docNode.children[1].child?.children.length).toEqual(3);
   });
 
-  // it("should parse variables in the header section", () => {
-  //   const input = `
-  //   `
-  // });
+  it("should parse variables in the header section", () => {
+    const input = `
+    ~ mum: "Mumbai"
+    ~ del: "Delhi"
+    ~ $details: {name: str, age: int, city: str}
+    --- people:$details
+    ~ "Alice", 25, @mum
+    ~ "Bob", 30, @del
+    `;
+
+    const tokenizer = new Tokenizer(input);
+    const tokens = tokenizer.tokenize();
+    const astParser = new ASTParser(tokens);
+    const docNode = astParser.parse();
+
+    // DocumentNode
+    expect(docNode instanceof DocumentNode).toEqual(true);
+    expect(docNode.children.length).toEqual(1);
+
+    // Header with 3 variables
+    expect(docNode.header?.child?.children.length).toEqual(3);
+    expect(docNode.header?.child?.children[0] instanceof ObjectNode).toEqual(
+      true
+    );
+    expect(docNode.header?.child?.children[1] instanceof ObjectNode).toEqual(
+      true
+    );
+    expect(docNode.header?.child?.children[2] instanceof ObjectNode).toEqual(
+      true
+    );
+
+    // Section
+    expect(docNode.children[0] instanceof SectionNode).toEqual(true);
+    expect(docNode.children[0].child?.children.length).toEqual(2);
+  });
 
   it("should parse schema and throw error because of incorrect syntax", () => {
+    // TODO: Update the test cases some of them are incorrect
     const input = [
       `~ $schema1: {a: int, b: int`,
       `a,b,c 
