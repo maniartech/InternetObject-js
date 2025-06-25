d81a537dce55e7982709dd6e5a9c1c8558bb5c10
Aamir Maniar <aamironline@gmail.com>
2025-06-25 18:28:27 +0530

    Reorder exports in index.ts for improved readability and consistency

--- src/index.ts
+++ src/index.ts
@@ -8,22 +8,20 @@
 //         ╚═╝  ╚═══════╝
 //         Internet Object
 
-export { default as Document } from './core/document';
-export { default as Definitions } from './core/definitions';
-export { default as SectionCollection } from './core/section-collection';
-export { default as Section } from './core/section';
-export { default as Collection } from './core/collection';
-export { default as InternetObject } from './core/internet-object';
-export { default as Schema } from './schema/schema';
-export { default as Decimal } from './core/decimal';
-export { default as InternetObjectError } from './errors/io-error';
-export { default as InternetObjectSyntaxError } from './errors/io-syntax-error';
+export { default as Collection                    } from './core/collection';
+export { default as Decimal                       } from './core/decimal';
+export { default as Definitions                   } from './core/definitions';
+export { default as Document                      } from './core/document';
+export { default as InternetObject                } from './core/internet-object';
+export { default as Section                       } from './core/section';
+export { default as SectionCollection             } from './core/section-collection';
+export { default as InternetObjectError           } from './errors/io-error';
+export { default as InternetObjectSyntaxError     } from './errors/io-syntax-error';
 export { default as InternetObjectValidationError } from './errors/io-validation-error';
+export { default as Schema                        } from './schema/schema';
 
 // Main tag functions (also tree-shakable)
-export { ioDocument } from './facade';
-export { ioObject } from './facade';
-export { ioDefinitions } from './facade';
+export { ioDefinitions, ioDocument, ioObject } from './facade';
 
 // Default facade (only imported if user does default import!)
 import io from './facade';
