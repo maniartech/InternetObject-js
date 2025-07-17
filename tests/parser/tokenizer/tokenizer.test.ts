980a96a9698bd35129fc509860a06892b8431a10
Aamir Maniar <aamironline@gmail.com>
2025-07-17 14:46:11 +0530

    fix: correct import statement for Decimal to ensure proper functionality in tokenizer tests

--- tests/parser/tokenizer/tokenizer.test.ts
+++ tests/parser/tokenizer/tokenizer.test.ts
@@ -1,5 +1,13 @@
 import Tokenizer from '../../../src/parser/tokenizer';
 import TokenType from '../../../src/parser/tokenizer/token-types';
 import Token from '../../../src/parser/tokenizer/tokens';
-import { Decimal } from '../../../src/core/decimal';
+import Decimal from '../../../src/core/decimal';
 
+describe('Tokenizer', () => {
+  test('should be defined', () => {
+    expect(Tokenizer).toBeDefined();
+    expect(TokenType).toBeDefined();
+    expect(Token).toBeDefined();
+    expect(Decimal).toBeDefined();
+  });
+});
