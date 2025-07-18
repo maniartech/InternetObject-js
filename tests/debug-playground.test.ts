c33fc16fd30a6972e003bcbaa9a206ea62b0281c
Aamir Maniar <aamironline@gmail.com>
2025-07-18 12:27:01 +0530

    Trial test renamed to debug playground

---
+++ tests/debug-playground.test.ts
@@ -0,0 +1,18 @@
+
+import Tokenizer from "../src/parser/tokenizer";
+import ASTParser from "../src/parser/ast-parser";
+
+// ⚠️ This is a test playhground to debug and try out new features and 
+// ⚠️ test cases.
+// ⚠️ It's not a part of the main test suite. It's just for testing
+// ⚠️ work in progress features and test cases. You may find some
+// ⚠️ broken test cases or incomplete, left out and commented code
+// ⚠️ and imports. After, successful debugging, clean up this file and reset.
+// ⚠️ 
+// ⚠️ Please ignore this file.
+describe('Trial - Failing Tests Only', () => {
+  it('FAILING TEST 1: Section Separator Error Recovery', () => {
+
+
+  });
+});
